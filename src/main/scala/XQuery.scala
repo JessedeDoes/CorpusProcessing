fe69e0a1f492afc50749010c5e414fa76e9ec850
jessededoes <jdedoes@gmail.com>
2017-10-16 15:21:26 +0200

    .

--- src/main/scala/XQuery.scala
+++ src/main/scala/XQuery.scala
@@ -1,34 +1,49 @@
-import org.openrdf.query.MalformedQueryException
-import org.openrdf.query.QueryLanguage
-import org.openrdf.query.parser.ParsedQuery
-import org.openrdf.query.parser.QueryParser
-import org.openrdf.query.parser.QueryParserUtil
-import it.unibz.inf.ontop.model.CQIE
-import it.unibz.inf.ontop.model.DatalogProgram
-import it.unibz.inf.ontop.model.Function
-import it.unibz.inf.ontop.model.Term
-import it.unibz.inf.ontop.model.Variable
-import it.unibz.inf.ontop.owlrefplatform.core.abox.SemanticIndexURIMap
-import it.unibz.inf.ontop.owlrefplatform.core.basicoperations.UriTemplateMatcher
-import it.unibz.inf.ontop.owlrefplatform.core.basicoperations.VocabularyValidator
-import it.unibz.inf.ontop.owlrefplatform.core.translator.DatalogToSparqlTranslator
-import it.unibz.inf.ontop.owlrefplatform.core.translator.SPARQLQueryFlattener
-import it.unibz.inf.ontop.owlrefplatform.core.translator.SparqlAlgebraToDatalogTranslator
 
+//import scala.collection.GenTraversableOnce
 
-class XPathTemplate(f: String=>String)
+class XPathTemplate(pathExpressions: Set[String])
 {
-  val pz = Set(
-    "$x←$y//entry/@id",
-    "$y←//entry",
-    "$z←$y::parent"
-  )
+
 }
 
-case class BasicPattern(pathExpressions: Set[String])
+case class BasicPattern(pathExpressions: Set[String], variables: Set[String])
 {
-  def join(b: BasicPattern) = BasicPattern(this.pathExpressions ++ b.pathExpressions)
+  def join(b: BasicPattern) = BasicPattern(this.pathExpressions ++ b.pathExpressions,
+    this.variables ++ b.variables)
+
+  def union(b: BasicPattern) =
+  {
+    val commonVariables = this.variables.intersect(b.variables)
+  }
+
+  lazy val pathMap:Map[String,String] = pathExpressions.map(p =>
+  {
+    val cols = p.split("←")
+    val (varName, path) = (cols(0), cols(1))
+    varName -> path
+  }).toMap
+
+  def varsIn(x: String)  = variables.filter(v => x.contains("$" + v))
+  def dep(v: String, p:String):List[(String,String)] = varsIn(p).map(v1 => (v,v1)).toList
+
+
+  lazy val dependencies = pathMap.map({case (v,p) => dep(v,p)}).flatten
+  //lazy val dependencies = dep0.map
+
+
+  lazy val variablesSorted = TopologicalSort.tsort(dependencies).toList
+
+  def toQuery() =
+  {
+    val dollar = "$"
+    val fors:String = variablesSorted.reverse.map(v => s"$dollar$v in ${pathMap(v)} ").mkString(",\n")
+    val re = variables.map(v => s"<var><name>$v</name><value>$dollar$v</value></var>").mkString("\n")
+    s"for  $fors \n return( <result>$re</result>) "
+   //  + "return () "
+  }
+  //def union
 }
+
 
 case class BasicMapping(property: String, subjectName: String, objectName: String)
 {
@@ -41,10 +56,20 @@ class XQuery {
 
 object x
 {
+  val t1 = BasicPattern(Set(
+    "x←$y//entry/@id",
+    "y←$u//entry",
+    "f←$y//form[@type='lemma']",
+    "u←document('data/wnt.xml')",
+    "z←$y::parent"
+  ), Set("u", "x", "y", "z","f"))
+
+
   def main(args: Array[String]) =
   {
-    val a = 1
-
+    //val a = 1
+    println(t1.toQuery)
+    //println(t1.pathExpressions.map(p => t1.varsIn(p)))
   }
 }
 
