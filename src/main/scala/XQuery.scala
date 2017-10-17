3a601b118fc83e6c69378a1a5cdefd9906e8096e
jessededoes <jdedoes@gmail.com>
2017-10-17 09:30:29 +0200

    .

--- src/main/scala/XQuery.scala
+++ src/main/scala/XQuery.scala
@@ -6,10 +6,14 @@ class XPathTemplate(pathExpressions: Set[String])
 
 }
 
-case class BasicPattern(pathExpressions: Set[String], variables: Set[String])
+trait XQueryNode
 {
+  val pathExpressions: Set[String]
+  val variables: Set[String]
   def join(b: BasicPattern) = BasicPattern(this.pathExpressions ++ b.pathExpressions,
     this.variables ++ b.variables)
+
+  def isSelected(v:String) = true
 
   def union(b: BasicPattern) =
   {
@@ -36,7 +40,7 @@ case class BasicPattern(pathExpressions: Set[String], variables: Set[String])
     val dollar = "$"
 
     val forPart:String = variablesSorted.map(v => s"$dollar$v in ${pathMap(v)} ").mkString(",\n")
-    val returnPart = variables.map(v => s"<var><name>$v</name><value>$dollar$v</value></var>").mkString("\n")
+    val returnPart = variables.filter(isSelected).map(v => s"<var><name>$v</name><value>{$dollar$v}</value></var>").mkString("\n")
 
     s"for  $forPart \n return( <result>$returnPart</result>) "
   }
@@ -48,13 +52,25 @@ case class BasicPattern(pathExpressions: Set[String], variables: Set[String])
         { case (v1,p) =>
           println(v + " in " + p)
           (v1, p.replaceAll("\\$" + v, m(v)) )}
-         )
+      )
     val mx = variablesSorted.foldLeft(pathMap)(foldie)
     mx.toString
   }
+}
+
+
+
+case class BasicPattern(pathExpressions: Set[String], variables: Set[String]) extends XQueryNode
+{
+
   // def union
 }
 
+case class SimpleSelect(pathExpressions: Set[String], variables: Set[String], selected: Set[String])
+  extends XQueryNode
+{
+  override def isSelected(v: String) = selected.contains(v)
+}
 
 case class BasicMapping(property: String, subjectName: String, objectName: String)
 {
@@ -76,11 +92,12 @@ object x
   ), Set("u", "x", "y", "z","f"))
 
 
-  val t2 = BasicPattern(Set(
+  val t2 = SimpleSelect(Set(
     "s←//node[@cat='smain']",
     "subj←$s/node[@rel='su']",
     "obj←$s/node[@rel='obj1']"
-  ), Set("s", "subj", "obj")
+  ), Set("s", "subj", "obj"),
+    Set("subj", "obj")
   )
   def main(args: Array[String]) =
   {
