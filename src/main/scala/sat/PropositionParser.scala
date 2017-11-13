937b6350ff65c6971803caf7a8dc7d86c60000c7
jessededoes <dedoes@xs4all.nl>
2017-11-13 09:07:59 +0100

    .

--- src/main/scala/sat/PropositionParser.scala
+++ src/main/scala/sat/PropositionParser.scala
@@ -20,13 +20,13 @@ trait PropositionParser extends JavaTokenParsers {
     }
 
   lazy val clause: Parser[Proposition] =
-    (expr ~ rep("[→↔]".r ~ expr)) ^^ {
+    (expression ~ rep("[→↔]".r ~ expression)) ^^ {
       case e ~ es => es.foldLeft(e) {
         case (t1, "→" ~ t2) => t1 → t2
       }
   }
 
-  lazy val expr: Parser[Proposition] =
+  lazy val expression: Parser[Proposition] =
     term ~ rep("[∨]".r ~ term)  ^^ {
       case t ~ ts => ts.foldLeft(t) {
         case (t1, "∨" ~ t2) => Or(t1, t2)
@@ -50,41 +50,4 @@ trait PropositionParser extends JavaTokenParsers {
   lazy val literal: Parser[Proposition] = "[A-za-z[0-9]_=]+".r ^^ { case s => Literal(s) }
 }
 
-/*
-trait ExprParsers2 extends JavaTokenParsers {
-
-  sealed abstract class Tree
-  case class Add(t1: Tree, t2: Tree) extends Tree
-  case class Sub(t1: Tree, t2: Tree) extends Tree
-  case class Mul(t1: Tree, t2: Tree) extends Tree
-  case class Div(t1: Tree, t2: Tree) extends Tree
-  case class Num(t: Double) extends Tree
-
-  def eval(t: Tree): Double = t match {
-    case Add(t1, t2) => eval(t1)+eval(t2)
-    case Sub(t1, t2) => eval(t1)-eval(t2)
-    case Mul(t1, t2) => eval(t1)*eval(t2)
-    case Div(t1, t2) => eval(t1)/eval(t2)
-    case Num(t) => t
-  }
-
-  lazy val expr: Parser[Tree] = term ~ rep("[+-]".r ~ term) ^^ {
-    case t ~ ts => ts.foldLeft(t) {
-      case (t1, "+" ~ t2) => Add(t1, t2)
-      case (t1, "-" ~ t2) => Sub(t1, t2)
-    }
-  }
-
-  lazy val term = factor ~ rep("[*]".r ~ factor) ^^ {
-    case t ~ ts => ts.foldLeft(t) {
-      case (t1, "*" ~ t2) => Mul(t1, t2)
-      case (t1, "/" ~ t2) => Div(t1, t2)
-    }
-  }
-
-  lazy val factor = "(" ~> expr <~ ")" | num
-
-  lazy val num = floatingPointNumber ^^ { t => Num(t.toDouble) }
-}
-
-*/
+// example combinator parser: https://gist.github.com/sschaef/5529436
