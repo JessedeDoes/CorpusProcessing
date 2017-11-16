7d0fca48f6cddad030705b345c606683ff39bc91
jessededoes <dedoes@xs4all.nl>
2017-11-16 08:20:05 +0100

    .

--- src/main/scala/folia/FoliaMapPosTags.scala
+++ src/main/scala/folia/FoliaMapPosTags.scala
@@ -9,14 +9,14 @@ case class FoliaMapPosTags(parseTag: String=>Tag, tagMapping: Tag => Tag) {
 
   val cachedMapping = Cache(tagMapping)
 
-  def updateDocument(e: Elem, condition: Elem=>Boolean, f: Elem => Elem):Elem =
+  def updateElement(e: Elem, condition: Elem=>Boolean, f: Elem => Elem):Elem =
   {
     if (condition(e))
       f(e)
     else
       e.copy(child = e.child.map({
         {
-          case e1: Elem => updateDocument(e1,condition,f)
+          case e1: Elem => updateElement(e1,condition,f)
           case n:Node => n
         }
       }))
@@ -28,10 +28,13 @@ case class FoliaMapPosTags(parseTag: String=>Tag, tagMapping: Tag => Tag) {
     val newTag = cachedMapping(tag)
     val features = newTag.features.map({case Feature(n,v) => <feat class={n} value={v}/>})
     e.copy(child=features,
-      attributes = e.attributes.remove("class").remove("confidence").append(new UnprefixedAttribute("class", newTag.toString, Null)) )
+      attributes = e.attributes
+        .remove("class")
+        .remove("confidence")
+        .append(new UnprefixedAttribute("class", newTag.toString, Null)) )
   }
 
-  def updatePoS(d: Elem):Elem = updateDocument(d, e => e.label == "pos", mapPosElement)
+  def updatePoS(d: Elem):Elem = updateElement(d, e => e.label == "pos", mapPosElement)
 
   def updatePoS(fileName: String):Unit =
     {
@@ -41,10 +44,12 @@ case class FoliaMapPosTags(parseTag: String=>Tag, tagMapping: Tag => Tag) {
     }
 }
 
+// ToDo deze manier van cachen is niet threadsafe
+
 case class Cache[A,B](f: A=>B)
 {
   val map = scala.collection.mutable.HashMap.empty[A,B]
-  def apply(a: A) = map.getOrElseUpdate(a, f(a))
+  def apply(a: A):B = map.getOrElseUpdate(a, f(a))
 }
 
 object TestSimplify
