12f9565b44e1d1b213b9e9e2ad4af98867ffea9f
jessededoes <dedoes@xs4all.nl>
2018-04-18 22:40:35 +0200

    .

--- src/main/scala/db2rdf/Ω.scala
+++ src/main/scala/db2rdf/Ω.scala
@@ -35,7 +35,7 @@ case class DataProperty(s: IRI, p: IRI, o: Literal) extends Statement
 
 trait Mapping
 
-case class mapping(p: IRI, s: ResultSet => IRI, o: ResultSet => IRI) extends Mapping
+case class Ω(p: IRI, s: ResultSet => IRI, o: ResultSet => IRI) extends Mapping
 {
   def triples(db: database.Database, q: String) : Stream[ObjectProperty] =
     {
@@ -43,7 +43,7 @@ case class mapping(p: IRI, s: ResultSet => IRI, o: ResultSet => IRI) extends Map
       db.stream(query)
     }
 }
-case class mappingDP(p: IRI, s: ResultSet => IRI, o: ResultSet => Literal) extends Mapping
+case class Δ(p: IRI, s: ResultSet => IRI, o: ResultSet => Literal) extends Mapping
 {
   def triples(db: database.Database, q: String) : Stream[DataProperty] =
     {
@@ -56,8 +56,8 @@ case class MultiMapping(mappings:Seq[Mapping])
 {
   def multiMapping(mappings:Seq[Mapping]): ResultMapping[Set[Statement]] =
   {
-    val dps = mappings.filter(_.isInstanceOf[mappingDP]).map(_.asInstanceOf[mappingDP]).toSet
-    val ops = mappings.filter(_.isInstanceOf[mapping]).map(_.asInstanceOf[mapping]).toSet
+    val dps = mappings.filter(_.isInstanceOf[Δ]).map(_.asInstanceOf[Δ]).toSet
+    val ops = mappings.filter(_.isInstanceOf[Ω]).map(_.asInstanceOf[Ω]).toSet
     ResultMapping(r => ops.map(x => ObjectProperty(x.s(r) ,x.p, x.o(r))) ++ dps.map(x => DataProperty(x.s(r) ,x.p, x.o(r)))  )
   }
 
@@ -71,7 +71,7 @@ case class MultiMapping(mappings:Seq[Mapping])
 
 
 
-object mapping {
+object Ω {
   implicit def x(s: String): ResultSet => IRI = r => IRI(r.getString(s))
 
   case class ϝ(field: String, f: String => String = identity)
@@ -159,44 +159,44 @@ object mapping {
     val sense = ~"http://rdf.ivdnt.org/sense/$persistent_id"
     val definition = ~"http://rdf.ivdnt.org/sense/$definition"
     ⊕(
-      mapping(subsense, ~"http://rdf.ivdnt.org/sense/$parent_id", sense),
-      mapping(lexicalDefinition, sense, definition),
-      mappingDP(definitionText, definition, !"definition")
+      Ω(subsense, ~"http://rdf.ivdnt.org/sense/$parent_id", sense),
+      Ω(lexicalDefinition, sense, definition),
+      Δ(definitionText, definition, !"definition")
     )
   }
 
-  val attestations = {
+  val attestations:MultiMapping = {
     val theAttestation = ~"http://attestation/$attestation_id"
     val document = ~"http://quotation/$document_id"
 
     ⊕(
-      mapping(attestation, ~"http://awf/$analyzed_wordform_id", theAttestation),
-      mapping(text, theAttestation, document),
-      mapping(attestation,  ~"http://rdf.ivdnt.org/sense/$sense_id", theAttestation),
-      mappingDP(beginIndex, theAttestation, r => IntLiteral(r.getInt("start_pos"))),
-      mappingDP(endIndex, theAttestation, r => IntLiteral(r.getInt("end_pos")))
+      Ω(attestation, ~"http://awf/$analyzed_wordform_id", theAttestation),
+      Ω(text, theAttestation, document),
+      Ω(attestation,  ~"http://rdf.ivdnt.org/sense/$sense_id", theAttestation),
+      Δ(beginIndex, theAttestation, r => IntLiteral(r.getInt("start_pos"))),
+      Δ(endIndex, theAttestation, r => IntLiteral(r.getInt("end_pos")))
     )
   }
 
-  val documents = {
+  val documents:MultiMapping = {
     val d = ϝ("document_id", "http://document/" + _)
     ⊕(
-      mappingDP("http://yearFrom", d, !"year_from"),
-      mappingDP("http://yearTo", d, !"year_to"),
-      mappingDP("http://title", d, !"title"),
-      mappingDP("http://author", d, !"author")
+      Δ("http://yearFrom", d, !"year_from"),
+      Δ("http://yearTo", d, !"year_to"),
+      Δ("http://title", d, !"title"),
+      Δ("http://author", d, !"author")
     )
   }
 
-  val posMapping =
+  val posMapping:MultiMapping =
     ⊕(
-      mapping(pos, ~"http//rdf.ivdnt.org/entry/$persistent_id",  ~"http://universaldependencies.org/u/pos/$lemma_part_of_speech")
+      Ω(pos, ~"http//rdf.ivdnt.org/entry/$persistent_id",  ~"http://universaldependencies.org/u/pos/$lemma_part_of_speech")
     )
 
-  val lemmata =
+  val lemmata:MultiMapping =
     ⊕(
-      mapping(canonicalForm, ~"http//rdf.ivdnt.org/entry/$persistent_id", ~"http://rdf.ivdnt.org/canonical/$lemma_id"),
-      mappingDP(writtenRep, ~"http://rdf.ivdnt.org/canonical/$lemma_id", !"modern_lemma"),
+      Ω(canonicalForm, ~"http//rdf.ivdnt.org/entry/$persistent_id", ~"http://rdf.ivdnt.org/canonical/$lemma_id"),
+      Δ(writtenRep, ~"http://rdf.ivdnt.org/canonical/$lemma_id", !"modern_lemma"),
     )
 
 
@@ -204,8 +204,8 @@ object mapping {
   {
     val awf = ~"http://awf/$analyzed_wordform_id"
     ⊕(
-      mapping(lexicalForm, ~"http//rdf.ivdnt.org/entry/$lemma_id", awf),
-      mappingDP(writtenRep, awf, !"wordform")
+      Ω(lexicalForm, ~"http//rdf.ivdnt.org/entry/$lemma_id", awf),
+      Δ(writtenRep, awf, !"wordform")
     )
   }
 
@@ -225,8 +225,8 @@ rel.id = r.getInt("id")// todo better id's (more persistent) for this
   {
     val synonymDef = ~"http//rdf.ivdnt.org/synonymdefinition/$id"
     ⊕(
-      mapping(synonymDefinition, ~"http//rdf.ivdnt.org/entry/$dictionary/$sense_id", synonymDef),
-      mappingDP(definitionText, synonymDef, !"synonym")
+      Ω(synonymDefinition, ~"http//rdf.ivdnt.org/entry/$dictionary/$sense_id", synonymDef),
+      Δ(definitionText, synonymDef, !"synonym")
 
       // ToDo doe de prov ellende hier ook
     )
@@ -242,7 +242,7 @@ rel.id = r.getInt("id")// todo better id's (more persistent) for this
     db.runStatement(("set schema 'data'"))
 
     synonyms.triples(db, synonymQuery).take(10).foreach(println)
-    posMapping.triples(db, posQuery).take(1000).foreach(println)
+    posMapping.triples(db, posQuery).take(10).foreach(println)
 
     allMappings.foreach(m =>
       m.triples(db, wordformQuery
