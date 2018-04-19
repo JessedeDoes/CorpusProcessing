270989c34e903dd26bc205e9fbd389dbba02b0b8
jessededoes <jesse.dedoes@ivdnt.org>
2018-04-19 09:53:00 +0200

    .

--- data/CG/combineer.scala
+++ data/CG/combineer.scala
@@ -1,6 +1,9 @@
+import scala.xml._
+
 object o
 {
- 
+  def linkje(p: String) = <a href={s"http://pcob67.inl.loc:8080/corpus-frontend/TestCRM/search?page=0&pageSize=_20&pattern=((name~poscode.case~false.value~_$p))&operation=hits"}>{p}</a>
+
   def main(args: Array[String]) = 
   { 
     val crmTags = scala.io.Source.fromFile("crm.log").getLines.map(x => x.split("\\s+").toList).map(x => x(0) -> x(1)).toMap
@@ -15,6 +18,28 @@ object o
 
     val allKeys = (crmTags.keySet ++ cg.keySet ++ morfcodes.keySet).toList.sorted
 
-    allKeys.foreach( k => println(s"$k\t${cg.getOrElse(k,"_")}\t${crmTags.getOrElse(k,"_")}\t${morfcodes.getOrElse(k,"_")}")   ) 
+//    allKeys.foreach( k => println(s"$k\t${cg.getOrElse(k,"_")}\t${crmTags.getOrElse(k,"_")}\t${morfcodes.getOrElse(k,"_")}")   ) 
+    
+    val xml = <table>
+              {allKeys.map(k =>
+                <tr>
+                  <td>
+                     {linkje(k)}
+                  </td>
+                   <td>
+                       {cg.getOrElse(k,"_")}
+                   </td>
+                   <td>
+                        {crmTags.getOrElse(k,"_")}
+                    </td>
+                    <td>
+                        {morfcodes.getOrElse(k,"_")}
+                    </td>
+                  </tr>)
+               }
+             </table>
+    
+
+             println(xml)
  }
 }
