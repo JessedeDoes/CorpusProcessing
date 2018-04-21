fa756c5b3db421618c4e60a0267d8d27bb09237f
jessededoes <dedoes@xs4all.nl>
2018-04-21 17:03:25 +0200

    .

--- src/main/scala/posmapping/CRM2Xml.scala
+++ src/main/scala/posmapping/CRM2Xml.scala
@@ -6,6 +6,33 @@ import utils.alignment.comp
 
 import scala.xml._
 
+case class Meta(locPlus: String, status: String, kloeke: String, year: String, month: String, id: String)
+{
+  def idPlus:String = s"$locPlus.$id".replaceAll(s"^${status}_",s"_$status:")
+  println(idPlus)
+
+  def interp(n:String, v:String):Elem  = <interpGrp type={n}><interp>{v}</interp></interpGrp>
+
+  val metaWithNames = List(
+    ("pid", uuid()),
+    ("witnessLocalization_kloeke", kloeke),
+    ("witnessYear_from", year),
+    ("titleLevel1", id))
+
+  def asXML:Elem = <listBibl type="metadata">
+    <bibl>
+      {metaWithNames.map({case (k,v) => interp(k,v)})  }
+    </bibl>
+  </listBibl>
+
+  def uuid():String =
+  {
+    val source = idPlus
+    val bytes = source.getBytes("UTF-8")
+    java.util.UUID.nameUUIDFromBytes(bytes).toString
+  }
+}
+
 object CRM2Xml {
   val atHome = true
   val dir = if (atHome) "/home/jesse/data/CRM/" else "/mnt/Projecten/Taalbank/CL-SE-data/Corpora/CRM/"
@@ -26,32 +53,7 @@ object CRM2Xml {
 
   def optXML(x: Option[Elem]):Elem = if (x.isEmpty) <none/> else x.get
 
-  case class Meta(locPlus: String, status: String, kloeke: String, year: String, month: String, id: String)
-  {
-    def idPlus:String = s"$locPlus.$id".replaceAll(s"^${status}_",s"_$status:")
-    println(idPlus)
 
-    def interp(n:String, v:String):Elem  = <interpGrp type={n}><interp>{v}</interp></interpGrp>
-
-    val metaWithNames = List(
-      ("pid", uuid()),
-      ("witnessLocalization_kloeke", kloeke),
-      ("witnessYear_from", year),
-      ("titleLevel1", id))
-
-    def asXML:Elem = <listBibl type="metadata">
-      <bibl>
-        {metaWithNames.map({case (k,v) => interp(k,v)})  }
-      </bibl>
-    </listBibl>
-
-    def uuid():String =
-    {
-      val source = idPlus
-      val bytes = source.getBytes("UTF-8")
-      java.util.UUID.nameUUIDFromBytes(bytes).toString
-    }
-  }
 
 
   def meta(c: Array[String]):Meta = { Meta(c(0), c(1), c(2), c(3), c(4), c(5))}
@@ -162,11 +164,32 @@ object CRM2Xml {
     pieces
   }
 
+  def makeGroupx[T](s: Stream[T], currentGroup:List[T], f: T=>Boolean):Stream[List[T]] =
+  {
+    if (s.isEmpty) Stream.empty
+    else if (f(s.head))
+      Stream.cons(currentGroup, makeGroup(s.tail, List(s.head), f))
+    else
+      makeGroup(s.tail, currentGroup :+ s.head, f)
+  }
+
+  def linkWithOriginal(s: Stream[Document], currentOriginal: Document):Stream[(Document, Document)] =
+  {
+    if (s.isEmpty) Stream.empty
+    else if (s.head.metadata.isEmpty)
+      Stream.cons( (s.head, currentOriginal), linkWithOriginal(s.tail, currentOriginal))
+    else Stream.cons( (s.head, s.head), linkWithOriginal(s.tail, s.head))
+  }
+
   def process():Unit =
   {
     val documents = makeGroup[Token](rawTokens, List.empty, _.isHeader)
       .filter(_.nonEmpty)
       .map(l => Document(l.head.lemma, l.tail))
+
+
+    val doSomethingWithN:Stream[(Document, Document)] = linkWithOriginal(documents, documents.head)
+    val xDocs = doSomethingWithN.map({case (d1,d2) => { d1.copy(metadata=Some(d2.me))}})
 
     val white = Text(" ")
     val xmlDocs = documents.map(
