8311adfa8038a76752882580411ad112778d0b06
jessededoes <dedoes@xs4all.nl>
2018-04-22 12:14:54 +0200

    .

--- src/main/scala/posmapping/CRM2Xml.scala
+++ src/main/scala/posmapping/CRM2Xml.scala
@@ -1,27 +1,60 @@
 package posmapping
 
 import org.incava.util.diff.Difference
+import posmapping.CRM2Xml.{kloekeByCode, optXML}
 import utils.{AlignmentGeneric, SimOrDiff}
 import utils.alignment.comp
 
 import scala.xml._
 
+
+case class Location(kloeke_code1: String, kloeke_code_oud: String, plaats: String, gemeentecode_2007: String, gemeente: String,
+                    streek: String, provincie: String, postcode: String, land: String, rd_x: String, rd_y: String, lat: String, lng: String,
+                    topocode: String, std_spelling: String, volgnr: String)
+
+object Location
+{
+  def makeLocation(a: Array[String]): Location =
+    Location(a(0), a(1), a(2), a(3), a(4), a(5), a(6), a(7), a(8), a(9), a(10), a(11), a(12), a(13), a(14), a(15))
+
+  def allKloekeCodes(f: String):Stream[Location] = scala.io.Source.fromFile(f).getLines().toStream.map(l => makeLocation(l.split("\\t")))
+
+}
+
+object Meta
+{
+  def interp(n:String, v:String):Elem  = <interpGrp type={n}><interp>{v}</interp></interpGrp>
+
+
+  def locationFields(c: String):NodeSeq =
+    optXML(
+      kloekeByCode.get(c).map(
+        l => Seq(
+          interp("witnessLocalization_province", l.provincie),
+          interp("witnessLocalization_place", l.plaats),
+          interp("witnessLocalization_lat", l.lat),
+          interp("witnessLocalization_long", l.lng),
+        )
+      ))
+}
+
 case class Meta(locPlus: String, status: String, kloeke: String, year: String, month: String, id: String)
 {
   def idPlus:String = s"$locPlus.$id".replaceAll(s"^${status}_",s"_$status:")
-  println(idPlus)
 
-  def interp(n:String, v:String):Elem  = <interpGrp type={n}><interp>{v}</interp></interpGrp>
+
 
   val metaWithNames = List(
     ("pid", uuid()),
+    ("witnessIsOriginalOrNot", status),
     ("witnessLocalization_kloeke", kloeke),
     ("witnessYear_from", year),
     ("titleLevel1", id))
 
-  def asXML:Elem = <listBibl type="metadata">
+  def asXML:NodeSeq = <listBibl type="metadata">
     <bibl>
-      {metaWithNames.map({case (k,v) => interp(k,v)})  }
+      {metaWithNames.map({case (k,v) => Meta.interp(k,v)})  }
+      {Meta.locationFields(kloeke)}
     </bibl>
   </listBibl>
 
@@ -33,36 +66,41 @@ case class Meta(locPlus: String, status: String, kloeke: String, year: String, m
   }
 }
 
+case class CRMTag(code: String, cgTag: String, cgnTag: String, description: String)
+
 object CRM2Xml {
   val atHome = true
   val dir = if (atHome) "/home/jesse/data/CRM/" else "/mnt/Projecten/Taalbank/CL-SE-data/Corpora/CRM/"
   val CRM:String = dir + "CRM14Alfabetisch.txt"
   val index:String = dir + "index"
+  val kloekeCodes = Location.allKloekeCodes(dir + "kloeke_cumul.csv")
+
+  val kloekeByCode = kloekeCodes.groupBy(_.kloeke_code1).mapValues(_.head)
+
+  import Meta._
+
+
   val squareCup = "⊔"
 
-  case class Tag(code: String, cgTag: String, cgnTag: String, description: String)
 
-  lazy val tags:Stream[Tag] = scala.io.Source.fromFile("data/CG/allTags.overzichtje.tsv")
+
+
+  lazy val tags:Stream[CRMTag] = scala.io.Source.fromFile("data/CG/allTags.overzichtje.tsv")
     .getLines.toStream.map(l => l.split("\\t"))
-    .map(c => Tag(c(0), c(1), c(2), c(3)) )
+    .map(c => CRMTag(c(0), c(1), c(2), c(3)) )
 
   lazy val tagMap:Map[String,String] = tags.groupBy(_.code).mapValues(_.head.cgTag)
 
   // o_I222p30601    o       I222p   1306    01      StBernardHemiksem.Summarium113.VlpNr6
   //@ @ @ _o:I222p30601.StBernardHemiksem.Summarium113.VlpNr6 Markup(samp) - - -
 
-  def optXML(x: Option[Elem]):Elem = if (x.isEmpty) <none/> else x.get
+  def optXML(x: Option[NodeSeq]):NodeSeq = if (x.isEmpty) <none/> else x.get
 
 
 
 
   def meta(c: Array[String]):Meta = { Meta(c(0), c(1), c(2), c(3), c(4), c(5))}
 
-  def metaFromId(s: String) =
-  {
-    val c = s.split(":")
-    val (status, rest) = (c(0), c(1))
-  }
 
   def getRows(fileName: String):Stream[Array[String]] = scala.io.Source.fromFile(fileName).getLines.toStream.map(_.split("\\s+"))
 
@@ -83,40 +121,43 @@ object CRM2Xml {
 
   def rewritePunc(s:String):String = puncMap.getOrElse(s, s)
 
+  def mapTag(codes: String) = codes.split("\\+").map(c => tagMap.getOrElse(c, "U" + c)).mkString("+")
+
   case class Token(word: String, wordLC: String, wordExpanded: String, lemma: String, tag: String)
   {
     def isHeader:Boolean = word.equals("@") && !tag.equals("Markup(line)")
 
     def isLine:Boolean = tag.equals("Markup(line)")
 
+    def isSeparator = tag.equals("Markup(sep)")
+
     def asXML:Elem =
-      if (isLine)
-        <lb/>
-      else
-      if (tag.contains("Punc"))
+      if (isLine) <lb/>
+      else if (isSeparator) <milestone type="separator"/> // ?? wat is dit precies
+      else if (tag.contains("Punc"))
         <pc>{rewritePunc(word)}</pc>
           else {
             val w = alignExpansionWithOriginal(word, wordExpanded)
-            <w lemma={lemma} type={tag} pos={tagMap.getOrElse(tag, tag)} orig={word} reg={wordExpanded}>{w}</w>
+            <w lemma={lemma} type={tag} pos={mapTag(tag)} orig={word} reg={wordExpanded}>{w}</w>
           }
   }
 
-  case class Document(id: String, tokens: List[Token])
+  case class Document(id: String, tokens: List[Token], metadata: Option[Meta])
   {
-    lazy val metadata:Option[Meta] = metaMap.get(id)
+
   }
 
   def token(c:Array[String]):Token = { Token(c(0), c(1), c(2), c(3), c(4)) }
   def token(s:String):Token = { val c = s.split("\\s+");  Token(c(0), c(1), c(2), c(3), c(4)) }
 
 
-  def makeGroup[T](s: Stream[T], currentGroup:List[T], f: T=>Boolean):Stream[List[T]] =
+  def makeGroupx[T](s: Stream[T], currentGroup:List[T], f: T=>Boolean):Stream[List[T]] =
   {
     if (s.isEmpty) Stream.empty
     else if (f(s.head))
-      Stream.cons(currentGroup, makeGroup(s.tail, List(s.head), f))
+      Stream.cons(currentGroup, makeGroupx(s.tail, List(s.head), f))
     else
-      makeGroup(s.tail, currentGroup :+ s.head, f)
+      makeGroupx(s.tail, currentGroup :+ s.head, f)
   }
 
   def alignExpansionWithOriginal(original: String, expansion: String):NodeSeq =
@@ -125,6 +166,7 @@ object CRM2Xml {
     val a = new AlignmentGeneric[Char](comp)
     val x = "~".r.findAllMatchIn(original).toStream.map(m => m.start).zipWithIndex.map(p => p._1 - p._2)
     val o1 = original.replaceAll("~","")
+
     val (diffs, sims) = a.findDiffsAndSimilarities(o1.toList, expansion.toList)
     val dPlus  = diffs.map(d => SimOrDiff[Char](Some(d.asInstanceOf[Difference]), None))
     val simPlus  = sims.map(s => SimOrDiff[Char](None, Some(s)))
@@ -164,35 +206,28 @@ object CRM2Xml {
     pieces
   }
 
-  def makeGroupx[T](s: Stream[T], currentGroup:List[T], f: T=>Boolean):Stream[List[T]] =
-  {
-    if (s.isEmpty) Stream.empty
-    else if (f(s.head))
-      Stream.cons(currentGroup, makeGroup(s.tail, List(s.head), f))
-    else
-      makeGroup(s.tail, currentGroup :+ s.head, f)
-  }
 
-  def linkWithOriginal(s: Stream[Document], currentOriginal: Document):Stream[(Document, Document)] =
+  def makeGroup[T](s: Stream[T], f: T=>Boolean):Stream[List[T]] =
   {
-    if (s.isEmpty) Stream.empty
-    else if (s.head.metadata.isEmpty)
-      Stream.cons( (s.head, currentOriginal), linkWithOriginal(s.tail, currentOriginal))
-    else Stream.cons( (s.head, s.head), linkWithOriginal(s.tail, s.head))
+     makeGroupx[T](s, List.empty, f).filter(_.nonEmpty)
   }
 
   def process():Unit =
   {
-    val documents = makeGroup[Token](rawTokens, List.empty, _.isHeader)
-      .filter(_.nonEmpty)
-      .map(l => Document(l.head.lemma, l.tail))
+    val documents:Stream[Document] = makeGroup[Token](rawTokens, t => t.isHeader)
+      .map(l => Document(l.head.lemma, l.tail, metaMap.get(l.head.lemma)))
 
-
-    val doSomethingWithN:Stream[(Document, Document)] = linkWithOriginal(documents, documents.head)
-    val xDocs = doSomethingWithN.map({case (d1,d2) => { d1.copy(metadata=Some(d2.me))}})
+    val withMetadataOriginal = makeGroup[Document](documents, d => d.metadata.isDefined)
+      .flatMap(g =>
+        {
+          val meta = g.head.metadata.get.copy(status="n")
+          g.head :: g.tail.map(x => x.copy(metadata=Some(meta.copy(id=x.id))))
+        }
+      )
 
     val white = Text(" ")
-    val xmlDocs = documents.map(
+
+    val xmlDocs = withMetadataOriginal.map(
       d =>
         {
           <TEI>
@@ -208,6 +243,7 @@ object CRM2Xml {
           </TEI>
         }
     )
+
     val corpus = <teiCorpus>{xmlDocs}</teiCorpus>
 
     val xml = CRM.replaceAll("txt$", "xml")
