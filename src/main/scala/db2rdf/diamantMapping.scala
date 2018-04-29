de6a1cbf292bc4153a8af22617bda5075427a7fc
jessededoes <dedoes@xs4all.nl>
2018-04-29 14:28:41 +0200

    .

--- src/main/scala/db2rdf/diamantMapping.scala
+++ src/main/scala/db2rdf/diamantMapping.scala
@@ -1,11 +1,12 @@
 package db2rdf
 
+import java.io.{FileOutputStream, FileWriter, OutputStreamWriter}
 import java.sql.ResultSet
+import java.util.zip.GZIPOutputStream
 
 import database.Configuration
 import db2rdf.Ω.{ϝ, ⊕}
 import db2rdf.IRI
-
 import org.slf4j._
 
 object posConversion {
@@ -229,6 +230,8 @@ object diamantMapping {
     import org.slf4j.Logger
     import org.slf4j.LoggerFactory
 
+    val outputFolder = args(0)
+
     System.setProperty("org.slf4j.simpleLogger.logFile", "/tmp/loggertje.log")
     System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "off")
     System.setProperty("log4j.rootLogge", "DEBUG, file")
@@ -239,35 +242,54 @@ object diamantMapping {
 
     //Console.err.println(s"######################################################lemmata en PoS voor ${lemmata.triplesIterator(db, lemmaQuery).size} lemmata")
 
-    //lemmata.triplesIterator(db, lemmaQuery).take(limit).foreach(println)
-    //posMapping.triplesIterator(db, posQuery).take(limit).foreach(println)
+    val lemmaOutput = new OutputStreamWriter(new GZIPOutputStream( new FileOutputStream((outputFolder + "/" + "lemmata.nt.gz"))))
+    val wordformOutput = new OutputStreamWriter(new GZIPOutputStream( new FileOutputStream((outputFolder + "/" + "wordforms.nt.gz"))))
+    val senseOutput = new OutputStreamWriter(new GZIPOutputStream( new FileOutputStream((outputFolder + "/" + "senses.nt.gz"))))
+    val attestationOutput = new OutputStreamWriter(new GZIPOutputStream( new FileOutputStream((outputFolder + "/" + "attestations.nt.gz"))))
+    val ezelOutput = new OutputStreamWriter(new GZIPOutputStream( new FileOutputStream((outputFolder + "/" + "ezels.nt.gz"))))
+    val serpensOutput = new OutputStreamWriter(new GZIPOutputStream( new FileOutputStream((outputFolder + "/" + "serpens.nt.gz"))))
 
+    lemmata.triplesIterator(db, lemmaQuery).take(limit).foreach(x => lemmaOutput.write(x.toString + "\n"))
+    posMapping.triplesIterator(db, posQuery).take(limit).foreach(x => lemmaOutput.write(x.toString + "\n"))
+
+    lemmaOutput.close()
 
     //Console.err.println(s"###################################################### woordvormen ${lemmaWordform.triplesIterator(db, wordformQuery).size} triples")
 
-    //lemmaWordform.triplesIterator(db, wordformQuery).take(limit).foreach(println)
+    lemmaWordform.triplesIterator(db, wordformQuery).take(limit).foreach(x => wordformOutput.write(x.toString + "\n"))
+
+    wordformOutput.close()
 
     Console.err.println(s"###################################################### senses synonym stuk: ${synonyms.triplesStream(db, synonymQuery).size}")
 
-    senses.triplesIterator(db, senseQuery).take(limit).foreach(println)
-    synonyms.triplesIterator(db, synonymQuery).take(limit).foreach(println)
+    senses.triplesIterator(db, senseQuery).take(limit).foreach(x => senseOutput.write(x.toString + "\n"))
+    synonyms.triplesIterator(db, synonymQuery).take(limit).foreach(x => senseOutput.write(x.toString + "\n"))
+
+    senseOutput.close()
 
     Console.err.println("###################################################### attestations en quotations")
 
-    quotations.triplesIterator(db, documentQuery).take(limit).foreach(println)
-    senseAttestations.triplesIterator(db, senseAttestationQuery).take(limit).foreach(println)
-    attestations.triplesIterator(db, attestationQuery).take(limit).foreach(println)
+    quotations.triplesIterator(db, documentQuery).take(limit).foreach(x => attestationOutput.write(x.toString + "\n"))
+    senseAttestations.triplesIterator(db, senseAttestationQuery).take(limit).foreach(x => attestationOutput.write(x.toString + "\n"))
+    attestations.triplesIterator(db, attestationQuery).take(limit).foreach(x => attestationOutput.write(x.toString + "\n"))
+
+    attestationOutput.close()
 
     Console.err.println("###################################################### ezels")
 
-    hilexSynsets.triplesIterator(db, synsetQuery).take(limit).foreach(println)
-    hilexSynsetRelations.triplesIterator(db, synsetRelationQuery).take(limit).foreach(println)
+    hilexSynsets.triplesIterator(db, synsetQuery).take(limit).foreach(x => ezelOutput.write(x.toString + "\n"))
+    hilexSynsetRelations.triplesIterator(db, synsetRelationQuery).take(limit).foreach(x => ezelOutput.write(x.toString + "\n"))
+
+    ezelOutput.close()
 
     Console.err.println("###################################################### serpens")
 
-    serpensConcepts.triplesIterator(db, serpensConceptQuery).take(limit).foreach(println)
-    serpensWNT.triplesIterator(db, serpensWntQuery).take(limit).foreach(println)
-    serpensConceptRelations.triplesIterator(db, conceptRelationQuery).take(limit).foreach(println)
+    serpensConcepts.triplesIterator(db, serpensConceptQuery).take(limit).foreach(x => serpensOutput.write(x.toString + "\n"))
+    serpensWNT.triplesIterator(db, serpensWntQuery).take(limit).foreach(x => serpensOutput.write(x.toString + "\n"))
+    serpensConceptRelations.triplesIterator(db, conceptRelationQuery).take(limit).foreach(x => serpensOutput.write(x.toString + "\n"))
+
+    serpensOutput.close()
+
   }
 }
 
