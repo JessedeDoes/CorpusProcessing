b3210ca85749982495161f2794faa34544eaf209
jessededoes <jesse.dedoes@ivdnt.org>
2018-06-05 15:58:58 +0200

    .

--- src/main/scala/posmapping/CGNMiddleDutch.scala
+++ src/main/scala/posmapping/CGNMiddleDutch.scala
@@ -1,11 +1,21 @@
 package posmapping
 
 
+/*
+N(soort,ev,form=0)
+N(soort,ev,form=-e)
+N(soort,ev,form=-s/-th)
+N(soort,ev,form=-t)
+N(soort,ev,form=-n)
+N(soort,ev,form=-r/-re)
+N(soort,ev,form=-a)
+N(soort,ev,form=other)
+ */
 object CGNMiddleDutch
 {
   //Console.err.println("eek!!")
   def partitions = Map(
-    "buiging"  -> Set("met-e", "met-s", "zonder"),
+    "buiging"  -> (Set("met-e", "met-s", "zonder") ++ Set("met-s-of-th", "met-t", "met-n", "met-r-of-re", "met-a", "overig")),
     "getal-n"  -> Set("mv-n", "zonder-n"),
     "lwtype"   -> Set("bep", "onbep"),
     "conjtype" -> Set("neven", "onder"),
@@ -31,7 +41,7 @@ object CGNMiddleDutch
   )
 
   def pos2partitions = List( // fix volgorde....
-    "N"    -> List("ntype", "getal", "graad", "genus", "naamval"),
+    "N"    -> List("ntype", "getal", "graad", "genus", "naamval",         "buiging"),
     "ADJ"  -> List("positie", "graad", "buiging", "getal-n", "naamval"),
     "WW"   -> List("wvorm", "pvtijd", "pvagr", "positie", "buiging", "getal-n"),
     "NUM"  -> List("numtype", "positie", "graad", "getal-n", "naamval"),
