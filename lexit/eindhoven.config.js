edaea4d42481239530a66c2f9cc1b97de991fd81
jessededoes <dedoes@xs4all.nl>
2018-06-17 12:08:21 +0200

    .

---
+++ lexit/eindhoven.config.js
@@ -0,0 +1,23 @@
+// list of tables that must be hidden or visible (don't use both, it's a matter of what's the most convenient)
+oHiddenTablesList = [];
+
+// table general settings
+oTableSettingsList = {
+		
+		
+		
+};
+
+var klusconfig = {
+
+                     mark: { "editable": true },
+                     comment: { "editable": true } 
+  };
+
+
+// configuration at column level
+oTableConfigurationList = {
+  zezij:  klusconfig,
+  adjectief: klusconfig
+};
+
