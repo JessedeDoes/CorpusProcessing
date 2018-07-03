c8b6ed0e9312409e39b33889fdaec4c19347f440
jessededoes <dedoes@xs4all.nl>
2018-07-03 21:15:51 +0200

    .

--- src/main/scala/CRM/allTags.scala
+++ src/main/scala/CRM/allTags.scala
@@ -3,55 +3,41 @@ package CRM
 import scala.xml._
 import utils.ProcessFolder
 
-object allPids
-{
-  def doFile(f: java.io.File):List[String] = (XML.loadFile(f) \\ "interpGrp")
+object allPids {
+  def main(args: Array[String]): Unit = {
+    def it(s: String) = s
+
+    val allFields = ProcessFolder.processFolder(new java.io.File(args(0)), doFile).flatten.toSet.toList.sortBy(it)
+    allFields.foreach(println)
+  }
+
+  def doFile(f: java.io.File): List[String] = (XML.loadFile(f) \\ "interpGrp")
     .filter(i => (i \\ "@type").text == "pid")
     .map(i => (i \ "interp").text.toString())
     .toList
-
-  def main(args: Array[String]): Unit = {
-    def it(s: String) =s
-    val allFields = ProcessFolder.processFolder( new java.io.File(args(0)), doFile).flatten.toSet.toList.sortBy(it)
-    allFields.foreach(println)
-  }
 }
 
-case class TaggedWord(id: String, gigantTag: String, cgnTag: String, morfcode: String, word: String, lemma: String)
-{
-  def tagjes = (cgnTag, morfcode, gigantTag)
+case class TaggedWord(id: String, gigantTag: String, cgnTag: String, morfcode: String, word: String, lemma: String) {
   lazy val codes = morfcode.split("\\+").toList
   lazy val lemmata = lemma.split("\\+").toList
   lazy val possen = gigantTag.split("\\+").toList
-  lazy val groupId = codes.find(_.contains("{")).map(_.replaceAll(".*\\{","").replaceAll("[}*#]",""))
+  lazy val groupId = codes.find(_.contains("{")).map(_.replaceAll(".*\\{", "").replaceAll("[}*#]", ""))
   lazy val starryIndex = codes.zipWithIndex.find(_._1.contains("{")).map(_._2)
   lazy val starryCode = starryIndex.map(codes(_))
   lazy val starryLemma = starryIndex.map(lemmata(_))
   lazy val starryPos = starryIndex.map(possen(_))
+
+  def tagjes = (cgnTag, morfcode, gigantTag)
 }
 
 
 // <w msd="WW(hulp-of-koppel,pv,tgw,met-t)" pos="VRB(type=auxiliary,finiteness=finite,tense=present,inflection=t)" type="213" lemma="hebben" xml:id="w.33228">
 object allTags {
 
-  def w2W(i : Node) = {
-    val word:String = i.child.filter(x => !(x.label == "fs")).text.trim
-    val id = i.attributes.filter(a => a.key == "id" || a.key=="n").head.value.text
-
-    TaggedWord(
-      id,
-      (i \ "@pos" ++ i \ "@function").text ,
-      (i \ "@msd").text,
-      (i \ "@type").text,
-      word,
-      (i \ "@lemma").text)
-  }
-
-  def doFile(f: java.io.File):List[TaggedWord] = { Console.err.println(f); (XML.loadFile(f) \\ "w").map(w2W).toList }
-
   def main(args: Array[String]): Unit = {
-    def it(s: String) =s
-    val allWords = ProcessFolder.processFolder( new java.io.File(args(0)), doFile).flatten
+    def it(s: String) = s
+
+    val allWords = ProcessFolder.processFolder(new java.io.File(args(0)), doFile).flatten
     val grouped = allWords.groupBy(w => w.tagjes)
     val withExamples = grouped.mapValues(l => (l.size, l.map(_.word).toSet.take(5))).toList.sortBy(_._1._1)
     withExamples.foreach(
@@ -63,10 +49,27 @@ object allTags {
     )
     //allFields.foreach(println)
   }
+
+  def doFile(f: java.io.File): List[TaggedWord] = {
+    Console.err.println(f);
+    (XML.loadFile(f) \\ "w").map(w2W).toList
+  }
+
+  def w2W(i: Node) = {
+    val word: String = i.child.filter(x => !(x.label == "fs")).text.trim
+    val id = i.attributes.filter(a => a.key == "id" || a.key == "n").head.value.text
+
+    TaggedWord(
+      id,
+      (i \ "@pos" ++ i \ "@function").text,
+      (i \ "@msd").text,
+      (i \ "@type").text,
+      word,
+      (i \ "@lemma").text)
+  }
 }
 
-object GysSettings
-{
+object GysSettings {
   val atHome = true
   val gysAtHome = "/mnt/DiskStation/homes/jesse/work/Gys/"
   val gysAtWork = "/mnt/Projecten/Nederlab/corpusbouw/TEI/CorpusGysseling/"
@@ -74,60 +77,78 @@ object GysSettings
   val gysInput = gysDir + "klaar-voor-Jesse-en-Mathieu/"
 }
 
-object sterretjeMatje
-{
-  import allTags._
+object sterretjeMatje {
 
-  def groupId(m: String) = m.replaceAll(".*\\{","").replaceAll("[}*#]","")
+  import allTags._
 
   val particleTF = io.Source.fromFile("data/Gys/particles.txt").getLines.map(l => l.split("\\t")).map(l => l(1) -> l(0).toInt).toMap
   val particles = particleTF.keySet
 
+  def groupId(m: String) = m.replaceAll(".*\\{", "").replaceAll("[}*#]", "")
 
-  def classify(l: Seq[TaggedWord]) =
-  {
+  def main(args: Array[String]): Unit = {
+    def it(s: String) = s
+
+    val allWords = ProcessFolder.processFolder(new java.io.File(GysSettings.gysInput), doFile).flatten.filter(_.morfcode.contains("{"))
+
+    val groups = allWords.groupBy(m => m.groupId)
+
+    groups.foreach({ case (id, g) =>
+
+      classify(g).zipWithIndex.foreach({ case (x, i) => println(s"[$i] $x") })
+    })
+  }
+
+  def classify(l: Seq[TaggedWord]) = {
     val h = l.head
 
     val code = h.starryCode.get
     val lemma = h.starryLemma.get
     val pos = h.starryPos.get
 
-    if (pos.contains("VRB"))
-      {
-        val particle = l.find(w => particles.contains(w.starryLemma.get.replaceAll("-","")))
-        val otherParts = particle.map(p => l.diff(Seq(p)))
-        Console.err.println(l.map(w => s"${w.morfcode}:${w.lemma}").mkString(" "))
-        Console.err.println(s"WW: $particle ////  $otherParts")
-        if (particle.isDefined)
-          List(particle.get.id -> "bw-deel-ww") ++ otherParts.getOrElse(List()).map(w => w.id -> "hoofddeel-ww")
-          else
-          List()
+    val plakLemma = l.map(_.starryLemma.get).mkString("|").replaceAll("-", "")
+
+    if (pos.contains("ADP")) {
+      l.zipWithIndex.map({case (w,i) =>
+          val feature = if (i == 0) "eerste-deel-omzetsel" else  "laatste-deel-omzetsel"
+          w.id -> (feature, w.word, plakLemma)
+      })
+    } else if (pos.contains("CON")) {
+      l.map(w => w.id -> ("deel-vg", w.word, plakLemma))
+    } else if (pos.matches("NOU.*proper.*")) {
+      l.map(w => w.id -> ("deel-eigen", w.word, plakLemma)) // zou spec(deeleigen) kunnen worden
+    } else if (pos.matches("NOU.*common.*")) {
+      l.map(w => w.id -> ("deel-znw", w.word, plakLemma))
+    } else if (pos.contains("VRB")) {
+      val particle = l.find(w => particles.contains(w.starryLemma.get.replaceAll("-", "")))
+      val otherParts = particle.map(p => l.diff(Seq(p)))
+
+      if (particle.isDefined) {
+        val lemma = (particle.get.starryLemma.get + "|" + otherParts.get.map(w => w.starryLemma.get).mkString("")).replaceAll("-", "")
+        List(particle.get.id -> ("bw-deel-ww", particle.get.word, lemma)) ++ otherParts.getOrElse(List()).map(w => w.id -> ("hoofddeel-ww", w.word, lemma))
       }
-    else if (pos.matches("ADV.*pron.*"))
-      {
-        val pronominalPart = l.find(_.starryLemma.get.matches(".*(HIER|DAAR|WAAR|ER).*"))
-        val otherParts = pronominalPart.map(p => l.diff(Seq(p)))
-        Console.err.println(l.map(w => s"${w.morfcode}:${w.lemma}").mkString(" "))
-        Console.err.println(s"ADVPRON: $pronominalPart ////  $otherParts")
-        if (pronominalPart.isDefined)
-          List(pronominalPart.get.id -> "hoofddeel-bw") ++ otherParts.getOrElse(List()).map(w => w.id -> "vz-deel-bw")
-        else
-          List()
-      } else
-      {
-          List()
+      else {
+        Console.err.println("WW UNCLEAR: " + l.map(w => s"${w.morfcode}:${w.lemma}").mkString(" "))
+        l.map(w => w.id -> ("deel", w.word, plakLemma))
       }
-  }
-  def main(args: Array[String]): Unit = {
-    def it(s: String) =s
+    } else if (pos.matches("ADV.*pron.*")) {
+      val pronominalPart = l.find(_.starryLemma.get.matches("-?(HIER|DAAR|WAAR|ER|ALDAAR)-?"))
+      val otherParts = pronominalPart.map(p => l.diff(Seq(p)))
 
-    val allWords = ProcessFolder.processFolder( new java.io.File(GysSettings.gysInput), doFile).flatten.filter(_.morfcode.contains("{"))
-
-    val groups = allWords.groupBy(m => m.groupId)
-
-    groups.foreach({case (id,g) =>
-
-      classify(g).foreach(println)
-    })
+      if (pronominalPart.isDefined) {
+        val lemma = (pronominalPart.get.starryLemma.get + "|" + otherParts.get.map(w => w.starryLemma.get).mkString("")).replaceAll("-", "")
+        List(pronominalPart.get.id -> ("hoofddeel-bw", pronominalPart.get.word, lemma)) ++ otherParts.getOrElse(List()).map(w => w.id -> ("vz-deel-bw", w.word, lemma))
+      }
+      else {
+        Console.err.println("VNWBW UNCLEAR: " + l.map(w => s"${w.morfcode}:${w.lemma}").mkString(" "))
+        l.map(w => w.id -> ("deel", w.word, plakLemma))
+      }
+    } else if (pos.matches("ADV.*")) {
+      l.map(w => w.id -> ("deel-bw", w.word, plakLemma)) // zou spec(deeleigen) kunnen worden
+    }
+    else {
+      Console.err.println("OTHER + " + l.map(w => s"${w.morfcode}:${w.lemma}").mkString(" "))
+      l.map(w => w.id -> ("deel", w.word, plakLemma))
+    }
   }
 }
