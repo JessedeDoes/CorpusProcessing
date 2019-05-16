7dea7ad12c146e916fd151801643c6f540d76340
jessededoes <jesse.dedoes@ivdnt.org>
2019-05-16 17:31:55 +0200

    .

--- src/main/scala/posmapping/mapMiddelnederlandseTags.scala
+++ src/main/scala/posmapping/mapMiddelnederlandseTags.scala
@@ -1,8 +1,197 @@
 package posmapping
 import scala.xml._
 import java.io.File
+
 import scala.util.matching.Regex._
 import scala.xml._
+import database.DatabaseUtilities.Select
+import database.{Configuration, Database}
+import posmapping.VMNWdb.QuotationReference
+
+/*
++----------------+-------------+------+-----+---------+-------+
+| Field          | Type        | Null | Key | Default | Extra |
++----------------+-------------+------+-----+---------+-------+
+| citaat_id      | int(11)     | NO   | MUL | 0       |       |
+| ldb_lemma_nr   | int(11)     | NO   |     | 0       |       |
+| citaat_plaats  | varchar(42) | YES  |     | NULL    |       |
+| woordvormtekst | varchar(80) | NO   |     |         |       |
+| woordvolgnr    | int(11)     | NO   |     | 0       |       |
+| doku_nr        | varchar(10) | NO   |     |         |       |
+| woordvorm_nr   | int(11)     | NO   | MUL | 0       |       |
+| bewijs_vanaf   | int(11)     | YES  |     | NULL    |       |
+| is_attestatie  | int(1)      | YES  |     | 0       |       |
+| onset          | int(11)     | YES  |     | NULL    |       |
+| is_clitic      | int(1)      | YES  |     | 0       |       |
++----------------+-------------+------+-----+---------+-------+
+
+ */
+
+object VMNWdb {
+  object vmnwconfig extends Configuration("vmnw", "svowdb02", "VMNW", "impact", "impact", driver="mysql")
+
+  val mainPoS = List("vnw.bw.", "bw.", "bnw.", "lidw.", "ww.", "znw.", "vnw.", "vw.", "vz.", "az.", "telw.")
+
+  case class QuotationReference(woordvorm_nr: Int, citaat_id: Int, ldb_lemma_nr: Int)
+
+
+  case class LemmaWoordvorm(ldb_lemma_nr: Int, woordvorm_nr: Int, lemmatekst_cg: String, lemmatekst_vmnw: String,
+                            woordsoort_afk: String, morfkode: Int, clitisch_deel_nr: Int, status_lemma_vmnw: Int, korte_bet: String, citaatLinks: Set[QuotationReference] = Set.empty)
+  {
+    lazy val mainPos: Option[String] = mainPoS.find(s => woordsoort_afk.contains(s))
+
+    lazy val compatible: Boolean = {
+      if (mainPos.isDefined) // ToDo also match features, e.g. relative pronoun etc
+        {
+          val ws = mainPos.get
+          ws match {
+            case "vnw.bw." => morfkode >= 600 & morfkode < 700
+            case "bw." => morfkode >= 500 & morfkode < 600
+            case "znw." => morfkode < 100
+            case "ww." => morfkode >= 200 & morfkode < 300
+            case "bnw." => morfkode >= 100 & morfkode < 200
+            case "telw." => morfkode >= 300 & morfkode < 400
+            case "vnw." => morfkode >= 400 & morfkode < 470 | morfkode >= 490 & morfkode < 500
+            case "lidw." => morfkode >= 400 & morfkode < 490
+            case "vz." => morfkode >= 700 & morfkode < 800
+            case "az." => morfkode >= 700 & morfkode < 800 // ?
+            case "vw." => morfkode >= 800 & morfkode < 900
+            case _ => true
+          }
+        } else true;
+    }
+
+    /*
+    +----------------+
+| woordsoort_afk |
++----------------+
+| aanw.vnw.      |
+| aanw.vnw.bw.   |
+| betr.vnw.      |
+| betr.vnw.bw.   |
+| bez.vnw.       |
+| onbep.vnw.     |
+| onbep.vnw.bw.  |
+| pers.vnw.      |
+| vnw.           |
+| vnw.bw.        |
+| vr.vnw.        |
+| vr.vnw.bw.     |
+| wdk.vnw.       |
+| wkg.vnw.       |
++----------------+
+
++----------------+
+| woordsoort_afk |
++----------------+
+| aanw.vnw.bw.   |
+| betr.vnw.bw.   |
+| onbep.vnw.bw.  |
+| vnw.bw.        |
+| vr.vnw.bw.     |
++----------------+
+
+     */
+    lazy val featureCompatible = compatible &&
+    {
+      mainPos match {
+        case Some("vnw.") =>
+          val klopt = if (woordsoort_afk.contains("aanw")) morfkode >= 410 & morfkode < 420
+          else if (woordsoort_afk.contains("betr")) morfkode >= 420 & morfkode < 440
+          else if (woordsoort_afk.contains("bez")) morfkode >= 450 & morfkode < 460
+          else if (woordsoort_afk.contains("onbep")) morfkode >= 440 & morfkode < 450
+          else if (woordsoort_afk.contains("pers")) morfkode >= 401 & morfkode < 407
+          else if (woordsoort_afk.contains("vr")) morfkode >= 420 & morfkode < 440
+          else if (woordsoort_afk.contains("wdk")) morfkode >= 460 & morfkode < 470
+          else if (woordsoort_afk.contains("wkg")) morfkode >= 460 & morfkode < 470
+          else true
+          klopt
+        case Some("vnw.bw.") =>
+          val klopt = if (woordsoort_afk.contains("aanw")) morfkode >= 610 & morfkode < 620
+          else if (woordsoort_afk.contains("betr")) morfkode >= 620 & morfkode < 640
+          //else if (woordsoort_afk.contains("bez")) morfkode >= 450 & morfkode < 460
+          else if (woordsoort_afk.contains("onbep")) morfkode >= 640 & morfkode < 650
+          else if (woordsoort_afk.contains("vr")) morfkode >= 620 & morfkode < 640
+          else true
+          klopt
+        case Some("bw.") =>
+          val klopt = if (woordsoort_afk.contains("aanw")) morfkode >= 510 & morfkode < 520
+          else if (woordsoort_afk.contains("betr")) morfkode >= 520 & morfkode < 540
+          //else if (woordsoort_afk.contains("bez")) morfkode >= 450 & morfkode < 460
+          else if (woordsoort_afk.contains("onbep")) morfkode >= 540 & morfkode < 550
+          else if (woordsoort_afk.contains("vr")) morfkode >= 520 & morfkode < 540
+          else true
+          klopt
+        case _ => true
+      }
+    }
+
+    lazy val supportedByAttestation = citaatLinks.exists(_.ldb_lemma_nr == this.ldb_lemma_nr)
+    lazy val element = <ref type="dictionary.VMNW" citaatId={citaatLinks.headOption.map(x => Text(x.citaat_id.toString))} n={clitisch_deel_nr.toString} target={ldb_lemma_nr.toString}>{lemmatekst_vmnw} [{korte_bet}] ({woordsoort_afk} {morfkode} compatible:{compatible},{featureCompatible} status:{status_lemma_vmnw})</ref>
+  }
+
+  val db = new Database(vmnwconfig)
+
+  lazy val lemmaWoordvormQuery = Select(r => LemmaWoordvorm(
+    r.getInt("ldb_lemma_nr"),
+    r.getInt("woordvorm_nr"),
+    r.getString("lemmatekst_cg").replaceAll("-[Xx]$","").toLowerCase(),
+    r.getString("lemmatekst_vmnw"),
+    r.getString("woordsoort_afk"),
+    r.getInt("morfkode"),
+    r.getInt("clitisch_deel_nr"),
+    r.getInt("status_lemma_vmnw"),
+    r.getString("korte_bet")
+  ), "lemma_woordvorm_view_alt")
+
+  lazy val citaatLinkQuery = Select(r => QuotationReference(
+    r.getInt("woordvorm_nr"),
+    r.getInt("citaat_id"),
+    r.getInt("ldb_lemma_nr")
+  ),   "citaat_tokens where is_attestatie=true")
+
+  lazy val citaatLinks = db.iterator(citaatLinkQuery)
+
+  lazy val citaatLinkMap: Map[Int,Set[QuotationReference]] = citaatLinks.toSet.groupBy(_.woordvorm_nr)
+
+  lazy val allemaal = db.iterator(lemmaWoordvormQuery)
+  lazy val woordvormLemma = {
+    val z = allemaal.toList.groupBy(_.woordvorm_nr)
+    Console.err.println("Yoho links collected!!!")
+    z
+  }
+
+  def prefer[T](s: Set[T], f: T => Boolean) = if (s.exists(f)) s.filter(f) else s
+
+  def preferList[T](s0: Set[T], fs: List[T => Boolean]) = fs.foldLeft(s0)({case (s,f) => prefer[T](s,f)})
+
+  def findDictionaryLinks(wordId: String) =
+  {
+    val woordvorm_nr = wordId.replaceAll("[^0-9]", "").toInt
+    val candidates = woordvormLemma.get(woordvorm_nr).getOrElse(Set())
+
+    val filtered0 = candidates.map(_.copy(citaatLinks = citaatLinkMap.getOrElse(woordvorm_nr, Set()))).toSet
+
+    val wouldBeNice:List[LemmaWoordvorm => Boolean] =
+      List(_.compatible,
+        _.status_lemma_vmnw == 0,
+        _.supportedByAttestation,
+        _.featureCompatible)
+
+    preferList(filtered0, wouldBeNice)
+  }
+
+  def linkXML(wordId: String) =
+  {
+    val l = findDictionaryLinks(wordId)
+    val n = l.size
+    if (l.isEmpty) <nolink/> else <xr type="dictionaryLinks" extent={n.toString}>{l.map(_.element).toSeq}</xr>
+  }
+
+  def main(args: Array[String]): Unit = {
+    allemaal.filter(x => x.mainPos.isEmpty).foreach(x => println(s"$x -->  ${x.mainPos}"))
+  }
+}
 
 object mapMiddelnederlandseTags extends mapMiddelnederlandseTagsClass(false)
 object mapMiddelnederlandseTagsGys extends mapMiddelnederlandseTagsClass(true)
@@ -174,8 +363,9 @@ class mapMiddelnederlandseTagsClass(gysMode: Boolean) {
 
     val featureStructures = makeFS(cgnTagFs) ++ makeFS(gysTagFS)
 
+    val dictionaryLinks:Seq[Node] = if (gysMode) VMNWdb.linkXML(newId.value.text) else Seq[Node]()
 
-    e.copy(attributes = withCompletedLemma, child = e.child ++ featureStructures)
+    e.copy(attributes = withCompletedLemma, child = e.child ++ featureStructures ++ dictionaryLinks)
   }
 
   def show(w: Node) = s"(${w.text},${(w \ "@lemma").text},${(w \ "@msd").text}, ${sterretjeMatje(w)})"
@@ -248,12 +438,12 @@ class mapMiddelnederlandseTagsClass(gysMode: Boolean) {
   {
     val children = w.child.zipWithIndex
 
-    val groupedChildren = makeGroup[(Node,Int)](children, {case (c,i) => !(c.isInstanceOf[Text] || c.label == "hi" || c.label == "expan")})
+    val groupedChildren = makeGroup[(Node,Int)](children, {case (c,i) => !(c.isInstanceOf[Text] || c.label == "hi" || c.label == "expan" || c.label=="c")})
 
     val newChildren = groupedChildren.flatMap(
       g => {
 
-        if (g.map(_._1.text).mkString.trim.nonEmpty && g.forall( {case (c,i) => c.isInstanceOf[Text] || c.label == "hi" || c.label == "expan"})) {
+        if (g.map(_._1.text).mkString.trim.nonEmpty && g.forall( {case (c,i) => c.isInstanceOf[Text] || c.label == "hi" || c.label == "expan" || c.label == "c"})) {
 
           val contents = removeUselessWhite(g).map(_._1)
           val ctext:String = contents.text.trim.replaceAll("\\s+", " ")
