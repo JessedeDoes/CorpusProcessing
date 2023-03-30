d2089ef52e496b5b7ee5fb1feacea34fd0aac860
jessededoes <jesse.dedoes@ivdnt.org>
2023-03-30 11:24:06 +0200

    .

--- src/main/scala/corpusprocessing/clariah_training_corpora/moderne_tagging/lassy/syntax_poc/QueryNode.scala
+++ src/main/scala/corpusprocessing/clariah_training_corpora/moderne_tagging/lassy/syntax_poc/QueryNode.scala
@@ -19,6 +19,14 @@ case class QueryNode(tokenProperties : TokenQuery, children: Set[QueryNode] = Se
 // [geneste haakjes? [pos="VERB" & lemma="fietsen" [rel="obj"] [rel="nsubj"]]
 // vergelijk https://ufal.mff.cuni.cz/pmltqdoc/doc/pmltq_tutorial_web_client.html
 
+/*
+Problemen:
+- Volgorde van dependents, ten opzichte van elkaar en ten opzichte van de head
+- Meerdere dependents met zelfde rol (bijvoorbeeld drie ADJ bij een znw)
+- 'Diepere' relaties (bijvoorbeeld  a ->* b voor b hangt willekeurig diep onder a, kan je constituenten mee maken)
+- In plaats van alleen maar captures iets boom-achtigs in het resultaat meegeven?
+ */
+
 object QueryNode {
   // Ja dit kan ook compacter met @* enzo....
    def q(a: TokenQuery) = QueryNode(a)
@@ -44,7 +52,7 @@ object QueryNode {
     )
 
   // [rel='root' & lemma='komen'
-  //      [rel='nsubj' & pos='NOUN' [pos='DET']]
+  //      x:[rel='nsubj' & pos='NOUN' [pos='DET']]
   //      [lemma='uit' & rel='compound:prt']
   //      [lemma='er']]
 
@@ -56,7 +64,11 @@ object QueryNode {
       q(LemmaQuery("er"))
     )
 
-     // [rel='root' [pos='NOUN' & rel='nsubj' [pos='ADJ']] [pos='NOUN' & rel='obj' [pos='ADJ']]]
+     // Subject en object hebben allebei een adjectief erbij:
+     // [rel='root'
+     //   [pos='NOUN' & rel='nsubj' [pos='ADJ']]
+     //   [pos='NOUN' & rel='obj' [pos='ADJ']]]
+
   val test2: QueryNode =
     q(
       TokenRelQuery("root"),
