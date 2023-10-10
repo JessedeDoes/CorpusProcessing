07296eb4dca9b407a5652464d2c9b3ba8bf2e210
jessededoes <jesse.dedoes@ivdnt.org>
2023-10-10 16:27:03 +0200

    minor

--- src/main/scala/corpusprocessing/clariah_training_corpora/quotation_corpora.scala
+++ src/main/scala/corpusprocessing/clariah_training_corpora/quotation_corpora.scala
@@ -10,9 +10,15 @@ case class QuotationCorpus(sourceFolder: String, name: String) extends tei_to_hu
   override val output_prefix: String = name
   override val max_files: Int = Integer.MAX_VALUE
   override val output_folder: String = sourceFolder + "/" + "test_train"
-  override val default_folder = sourceFolder
+  new java.io.File(output_folder).mkdir()
+  override val default_folder = sourceFolder + "/" + "CobaltServeExport"
 }
 
 object QuotationCorpora {
-  object q18 extends QuotationCorpus()
+  val baseDir  = "/mnt/Projecten/Corpora/TrainingDataForTools/galahad-corpus-data/public-corpora/clariah-evaluation-corpora/"
+
 }
+
+import QuotationCorpora._
+object q18 extends QuotationCorpus(baseDir + "gtbcit_18", "gtbcit_18")
+object q17 extends QuotationCorpus(baseDir + "gtbcit_17", "gtbcit_17")
