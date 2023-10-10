1fe910ac16f149ba40f0bfcef6c2474243f73901
JessedeDoes <dedoes@xs4all.nl>
2023-10-10 19:11:43 +0200

    minor

---
+++ src/main/scala/zeeuws/zeeuws_xml_to_db.scala
@@ -0,0 +1,33 @@
+package zeeuws
+
+import scala.xml._
+import utils.PostProcessXML._
+object zeeuws_xml_to_db {
+   val filename = "/mnt/Projecten/Hercules/DSDD/WZD/selectie.xml"
+   lazy val doc = XML.load(filename)
+   def markRegions(s: Elem): Elem = {
+     val children = s.child.zipWithIndex
+
+     val nc = children.map({case (c,i) =>
+       c match {
+         case e: Elem if e.label == "placeName" =>
+           val after = children.drop(i+1).map(_._1).text.trim
+           println("####\n" + e)
+           println(after)
+           if (after.startsWith("(")) {
+             e.copy(label="lamePlaceName")
+           } else
+             e
+         case _ => c
+       }
+     })
+     s.copy(child = nc)
+   }
+
+  lazy val postprocessedDoc = {
+    updateElement(doc, _.label=="usg", markRegions)
+  }
+  def main(args: Array[String]) = {
+    println(postprocessedDoc)
+  }
+}
