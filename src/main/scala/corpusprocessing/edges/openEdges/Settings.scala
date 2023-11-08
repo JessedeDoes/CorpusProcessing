a09d457cca83549ecac9a41bc4b61aff006e744a
JessedeDoes <dedoes@xs4all.nl>
2023-11-08 14:38:54 +0100

    minor

--- src/main/scala/corpusprocessing/edges/openEdges/Settings.scala
+++ src/main/scala/corpusprocessing/edges/openEdges/Settings.scala
@@ -5,7 +5,8 @@ object Settings {
    val baseDirAtWork = "/mnt/Projecten/Corpora/Historische_Corpora/EDGeS_historical_bible_corpus/OpenEDGeS/OpenEDGeS/"
    val baseDirAtHome = "/media/jesse/Data/Corpora/Edges/OpenEDGeS/"
    val baseDir = baseDirAtHome
-   val teiDir = "/media/jesse/Data/Corpora/Edges/TEI"
+   val teiDir = "/media/jesse/Data/Corpora/Edges/TEI/"
+   val tokenizedContentDir = teiDir + "ids-fixed/"
    val nl_nl = baseDir + "Alignments/nl-nl/"
    val en_nl = baseDir + "Alignments/en-nl/"
    val staten_canisius = nl_nl + "nl_1637_Staten-nl_1939_Canisius.tsv"
---
+++ src/main/scala/corpusprocessing/edges/openEdges/sampleVerses/sampling.scala
@@ -0,0 +1,54 @@
+package corpusprocessing.edges.openEdges.sampleVerses
+import corpusprocessing.edges.openEdges.Settings.{en_nl, teiDir, tokenizedContentDir}
+import corpusprocessing.clariah_training_corpora.fixTokenization.getId
+import scala.xml._
+import java.io.File
+import utils.PostProcessXML._
+object sampling {
+    val maxBooks = Integer.MAX_VALUE
+    val maxTokens = 5000
+    lazy val allFiles = new File(tokenizedContentDir).listFiles().filter(x => x.getName.endsWith(".xml") && x.getName.startsWith("nl") ).toList
+    def bible(f: File) = f.getName.replaceAll("\\.[^.]+\\.xml$", "")
+    def lineNumber(verse: Node) = (getId(verse).replaceAll(".*\\.", "")).toInt
+
+    lazy val bibles: Map[String, List[File]] = allFiles.groupBy(bible).mapValues(_.take(maxBooks))
+
+    def bibleVerses(bible:  String): List[(String, Node)] = bibles(bible).flatMap(f => {
+        val name = f.getCanonicalPath
+        val x = XML.loadFile(f)
+        (x \\ "ab").map(ab => name -> ab)
+    })
+
+    def selectVerses(bible: String, maxTokens: Int=sampling.maxTokens)  = {
+        val verses = bibleVerses(bible)
+        val shuffled: Seq[(String, Node)] = util.Random.shuffle(verses)
+        var cumul: Int = 0
+        lazy val selection: Seq[(String, Node)] = shuffled.takeWhile({case (b, verse: Node)  =>
+            cumul = cumul + (verse \\ "w").size
+            cumul <= maxTokens
+        })
+        selection
+    }
+
+    def main(args: Array[String])  = {
+        val sampleDir = teiDir + "/Samples/"
+
+        bibles.foreach({case (b, files) =>
+           val sample: Map[String, Seq[Node]] = selectVerses(b).groupBy(_._1).mapValues(_.map(_._2))
+            // dit toch liever per book doen
+
+            {
+                sample.foreach { case (f, verses) =>
+                    val doc = XML.load(f)
+                    val header = doc \\ "teiHeader"
+                    val doc1 = updateElement(doc, _.label == "body", x => <body><div>
+                        {verses.sortBy(lineNumber)}
+                    </div></body>)
+                    val saveTo =  sampleDir + ""  + new File(f).getName
+                   println(saveTo)
+                   XML.save(saveTo, doc1)
+                }
+            }
+        })
+    }
+}
