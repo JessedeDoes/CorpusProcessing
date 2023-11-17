51446f296811534d7c24f65955e6fdcd56ae476e
JessedeDoes <dedoes@xs4all.nl>
2023-11-17 23:04:24 +0100

    minor

--- src/main/scala/zeeuws/shortrackonline.scala
+++ src/main/scala/zeeuws/shortrackonline.scala
@@ -11,7 +11,7 @@ case class Competition(n: Node) {
 
 }
 
-case class Skater(n: Node, name: String, club: String) {
+case class Skater(n: Node, name: String, club: String, number: String) {
   //println("######")
   //println((n \\ "td").map(_.text.trim).filter(_.nonEmpty).mkString("\n"))
   val pretty = new scala.xml.PrettyPrinter(100,2)
@@ -66,13 +66,13 @@ object shortrackonline {
     val src = Source.fromFile("aapje")
   }
 
-  def getSkater(url: String, name: String, club: String) = {
+  def getSkater(url: String, name: String, club: String, number: String) = {
     Console.err.println("Try skater: " + url)
     val lines  = io.Source.fromURL(new URL(url.replaceAll("http","https"))).getLines()
     // Console.err.println(lines.toList)
     val content = lines.mkString("\n")
     val contentX = HTML.parse(content)
-    Skater(contentX, name, club)
+    Skater(contentX, name, club, number)
   }
 
   val lewis = "http://www.shorttrackonline.info/skaterbio.php?id=STNED12802201301"
@@ -93,18 +93,22 @@ object shortrackonline {
   val pretty = new scala.xml.PrettyPrinter(300, 4)
   val distances = List(222, 333, 444, 500, 777, 1000, 1500)
 
+  def text(x: String)  = XML.loadString(s"<e>$x</e>").child
+  lazy val nbsp = text("&#160;")
+  def times(n: Int, node: NodeSeq) = (0 to n-1).flatMap(i => node)
   def main(args: Array[String]): Unit = {
     val htmlToProcess = args.headOption.getOrElse("data/regio.html")
     val fAll: Skater => Boolean = x => true;
     val fIHCL: Skater => Boolean = x => x.club.toLowerCase().contains("ihcl");
 
-    val tasks = List("/tmp/allSkaters.html" -> fAll, "/tmp/ihclOnly.html" -> fIHCL)
+    val tasks = List("/tmp/ihclOnly.html" -> fIHCL) // "/tmp/allSkaters.html" -> fAll,
 
     tasks.foreach({ case (fileName, filter) =>
       val regio = HTML.parse(io.Source.fromFile(htmlToProcess).getLines().mkString("\n"))
       // println(pretty.format(regio))
       val sections = (regio \\ "h3").map(_.text)
       //println(sections)
+      var allSkaters = List[Skater]()
       val tablez = sections.map(s => {
         val parent = regio.descendant.filter(x => ((x \ "h3").exists(_.text == s))).head
         val childElems = parent.child.filter(_.isInstanceOf[Elem])
@@ -113,10 +117,11 @@ object shortrackonline {
         //println(s)
 
         val skaters: immutable.Seq[Skater] = (next \\ "tr").flatMap(tr => {
+          val number = (tr \ "td") (1).text
           val name = (tr \ "td") (3).text
-          val club = (tr \ "td") (4).text.trim
+          val club = (tr \ "td") (4).text.trim.replaceAll("^[A-Za-z0-9]", "")
           (tr.descendant.filter(x => (x \ "@href").nonEmpty)
-            .map(x => getSkater((x \ "@href").text, name, club))
+            .map(x => getSkater((x \ "@href").text, name, club, number))
             )
         }).sortBy(s => s.PR500)
 
@@ -124,6 +129,7 @@ object shortrackonline {
         val infos = skaters.filter(filter).map(skater => {
           skater.htmlRow
         })
+        allSkaters = allSkaters ++ skaters.filter(filter)
         println(s + ": " + infos.size)
         <div>
           <h3>
@@ -144,11 +150,39 @@ object shortrackonline {
             tr {{ height: 13pt }}
             table, tr, td, th {{ border: 1px solid black;
             padding: 3px;
-            border-collapse: collapse; }}
+            border-collapse: collapse;
+            font-size: 12pt;
+            }}
             body {{ font-family : calibri; font-size: 11px }}
           </style>
         </head> <body>
+          <div>
           {tablez}
+          </div>
+          <div>
+            {allSkaters.grouped(2).map(g => {
+              <table>
+                <tr>
+                  {g.map( s => <td><div style="font-size:12pt">
+                  <h3>
+                    {s.number} {s.name}
+                  </h3>
+                  <table style="border-collapse: collapse; border-style:solid; border-width:1pt">
+                    {(0 to 2).map(i => <tr>
+                    <td width="2em">
+                      {times(3, nbsp)}
+                    </td> <td width="16em">
+                      {times(25, nbsp)}
+                    </td> <td width="20em">
+                      {times(35, nbsp)}
+                    </td>
+                  </tr>)}
+                  </table>
+                </div></td>)}
+                </tr>
+              </table>
+             })}
+          </div>
         </body>
       </html>
       XML.save(fileName, xml)
