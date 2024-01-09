2cd3d83765977e08f6ffdfe9fcbd9556e957aa50
jessededoes <jesse.dedoes@ivdnt.org>
2024-01-09 15:23:41 +0100

    minor

---
+++ src/main/scala/corpusprocessing/clariah_training_corpora/enhanceBaB/tweakNELemmata.scala
@@ -0,0 +1,28 @@
+package corpusprocessing.clariah_training_corpora.enhanceBaB
+
+import scala.xml._
+import utils.PostProcessXML._
+import utils.ProcessFolder._
+
+
+// Dit hoeft dus helemala niet. Niet doen!!
+object tweakNELemmata {
+  val babPath = "/mnt/Projecten/Corpora/Historische_Corpora/BrievenAlsBuit/SelectionForEnhancementTagged/"
+  val outputPath = "/mnt/Projecten/Corpora/Historische_Corpora/BrievenAlsBuit/SelectionForEnhancementTweaked/"
+  implicit def f(fn: String)  = new java.io.File(fn)
+
+  def dow(w: Elem)  = {
+    if ((w \ "@pos").text.contains("NOU-P")) println(w \ "@lemma" + " : " + w \ "@pos")
+    w
+  }
+  def tweakLemmata(in: String, out: String)  = {
+    val d = XML.load(in)
+    val d1 = updateElement(d, _.label == "w", dow)
+    // println(out)
+    XML.save(out, d1)
+  }
+
+  def main(args: Array[String])  = {
+    processFolder(babPath, outputPath, tweakLemmata)
+  }
+}
