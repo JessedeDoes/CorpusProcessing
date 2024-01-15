7d43eca687a8cbb647b56ada5d587770116e3904
JessedeDoes <dedoes@xs4all.nl>
2024-01-15 17:12:23 +0100

    minor

--- src/main/scala/corpusprocessing/GCND/GCNDDatabase.scala
+++ src/main/scala/corpusprocessing/GCND/GCNDDatabase.scala
@@ -7,53 +7,6 @@ import java.io.PrintWriter
 import scala.xml._
 
 
-/*
-Problemen:
-- foutmeldingen bij UD conversie - tokens zonder makkelijk vindbaar head
-/home/jesse/workspace/XmlToRdf/data/GCND/Alpino/speech.alpino.100.xml
-  sentence ID: H016p_3--H016p_3_1--0315
-  error: No internal head for 13:voo
-    in internalHeadPosition
-        node -- id:22  begin:13  end:22  cat:ti  rel:body
-    in internalHeadPositionWithGapping
-        node -- id:22  begin:13  end:22  cat:ti  rel:body
-    in externalHeadPosition
-        node -- id:21  begin:12  end:13  word:voo  pt:vz  rel:cmp  lemma:om
-    in addDependencyRelations
-        node -- id:21  begin:12  end:13  word:voo  pt:vz  rel:cmp  lemma:om
-/home/jesse/workspace/XmlToRdf/data/GCND/Alpino/speech.alpino.10.xml
-  sentence ID: H016p_3--H016p_3_1--0487
-  error: No external head for 9:hé
-    in externalHeadPosition
-        node -- id:12  begin:8  end:9  word:hé  pt:tsw  rel:tag  lemma:hé
-    in addDependencyRelations
-        node -- id:12  begin:8  end:9  word:hé  pt:tsw  rel:tag  lemma:hé
-
-- lemmatisering bij instructies als [ @alt om voor ], je krijgt dan lemma 'om' bij 'voor'
-
-- elan_annotatie zonder tekst_lv
-
-gcnd=# select count(*) from elan_annotatie where tekst_lv is null;
- count
--------
-    94
-(1 row)
-
-- lichte en zware vervlaamsing moeilijk te aligneren
-
-De Alpino analyses zijn niet helemaal volgens de Lassy guidelines: (2x hd bijvoorbeeld)
-
-
-<node begin="8" cat="np" end="10" id="7" rel="su">
-    <node isWord="true" end="9" begin="5" buiging="zonder" graad="basis" id="8" lemma="weinig" naamval="stan" npagr="agr" pdtype="grad" pos="adj" positie="prenom" postag="VNW(onbep,grad,stan,prenom,zonder,agr,basis)" pt="vnw" rel="hd" root="weinig" sense="weinig" vwtype="onbep" word="weinig"/>
-    <node isWord="true" end="10" begin="6" getal="mv" graad="basis" id="9" lemma="schip" ntype="soort" pos="noun" postag="N(soort,mv,basis)" pt="n" rel="hd" root="schip" sense="schip" word="schepen"/>
-</node>
-
-
-Weinig zou een det relatie met schip moeten hebben.
-Zo komen weinig en schip er allebei als "su" uit in de pure dependenties
-
- */
 
 object GCNDDatabase {
   val doAll = true
@@ -214,3 +167,51 @@ def getPseudoFoLiAForAlpinoAnnotations(transcriptie_id: Int) =
 </FoLiA>
 
 */
+
+/*
+Problemen:
+- foutmeldingen bij UD conversie - tokens zonder makkelijk vindbaar head
+/home/jesse/workspace/XmlToRdf/data/GCND/Alpino/speech.alpino.100.xml
+  sentence ID: H016p_3--H016p_3_1--0315
+  error: No internal head for 13:voo
+    in internalHeadPosition
+        node -- id:22  begin:13  end:22  cat:ti  rel:body
+    in internalHeadPositionWithGapping
+        node -- id:22  begin:13  end:22  cat:ti  rel:body
+    in externalHeadPosition
+        node -- id:21  begin:12  end:13  word:voo  pt:vz  rel:cmp  lemma:om
+    in addDependencyRelations
+        node -- id:21  begin:12  end:13  word:voo  pt:vz  rel:cmp  lemma:om
+/home/jesse/workspace/XmlToRdf/data/GCND/Alpino/speech.alpino.10.xml
+  sentence ID: H016p_3--H016p_3_1--0487
+  error: No external head for 9:hé
+    in externalHeadPosition
+        node -- id:12  begin:8  end:9  word:hé  pt:tsw  rel:tag  lemma:hé
+    in addDependencyRelations
+        node -- id:12  begin:8  end:9  word:hé  pt:tsw  rel:tag  lemma:hé
+
+- lemmatisering bij instructies als [ @alt om voor ], je krijgt dan lemma 'om' bij 'voor'
+
+- elan_annotatie zonder tekst_lv
+
+gcnd=# select count(*) from elan_annotatie where tekst_lv is null;
+ count
+-------
+    94
+(1 row)
+
+- lichte en zware vervlaamsing moeilijk te aligneren
+
+De Alpino analyses zijn niet helemaal volgens de Lassy guidelines: (2x hd bijvoorbeeld)
+
+
+<node begin="8" cat="np" end="10" id="7" rel="su">
+    <node isWord="true" end="9" begin="5" buiging="zonder" graad="basis" id="8" lemma="weinig" naamval="stan" npagr="agr" pdtype="grad" pos="adj" positie="prenom" postag="VNW(onbep,grad,stan,prenom,zonder,agr,basis)" pt="vnw" rel="hd" root="weinig" sense="weinig" vwtype="onbep" word="weinig"/>
+    <node isWord="true" end="10" begin="6" getal="mv" graad="basis" id="9" lemma="schip" ntype="soort" pos="noun" postag="N(soort,mv,basis)" pt="n" rel="hd" root="schip" sense="schip" word="schepen"/>
+</node>
+
+
+Weinig zou een det relatie met schip moeten hebben.
+Zo komen weinig en schip er allebei als "su" uit in de pure dependenties
+
+ */
