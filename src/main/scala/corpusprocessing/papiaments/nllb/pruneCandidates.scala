4cd2f9f1d75694d8e50c8f2e575317dbcdc5807a
JessedeDoes <dedoes@xs4all.nl>
2024-02-05 23:02:37 +0100

    minor

--- src/main/scala/corpusprocessing/papiaments/nllb/pruneCandidates.scala
+++ src/main/scala/corpusprocessing/papiaments/nllb/pruneCandidates.scala
@@ -1,35 +1,51 @@
 package corpusprocessing.papiaments.nllb
 
-import java.io.PrintWriter
+import java.io.{BufferedInputStream, FileInputStream, PrintWriter}
+import java.util.zip.GZIPInputStream
 
 object pruneCandidates {
-
+  val pairs = "/mnt/Projecten/Papiaments/Alignments/pairs.out.gz"
   def list(words: List[(String,Int)])  = words.map({case (w,i) => s"$w: $i"}).mkString(";")
   def main(args: Array[String])  = {
-     val p = pruneCandidates("/tmp/pairs.out")
-     val out = new PrintWriter("/tmp/pairs.pruned.out")
+     val p = pruneCandidates(args.headOption.getOrElse(pairs))
+     val out_e2p = new PrintWriter("/tmp/pairs.pruned.e2p.out")
+     val out_p2e = new PrintWriter("/tmp/pairs.pruned.p2e.out")
+
      p.leftWords.foreach(w => {
-       Console.err.println(w.word)
-       out.println(s"${w.word}:${w.count} ${list(w.prunedCandidates)} -unpruned- ${list(w.candidates.take(2))}")
+       // Console.err.println(w.word)
+       out_e2p.println(s"${w.word}:${w.count} ${list(w.prunedCandidates)} -unpruned- ${list(w.candidates.take(2))}")
      })
-    out.close()
+     out_e2p.close()
+
+     p.rightWords.foreach(w => {
+      // Console.err.println(w.word)
+      out_p2e.println(s"${w.word}:${w.count} ${list(w.prunedCandidates)} -unpruned- ${list(w.candidates.take(2))}")
+     })
+     out_e2p.close()
+     out_p2e.close()
   }
 }
 
 
 case class Word(theOtherSide:Mapped, word: String, candidates: List[(String,Int)]) {
+
+  val minPart = 10
   lazy val count = candidates.map(_._2).sum
   def prunedCandidates = candidates.filter(x => {
     val otherCount = theOtherSide.count(x._1)
     val c = x._2
-    5 * c > otherCount && 5 * otherCount > this.count
+    minPart * c > otherCount && minPart * otherCount > this.count
   })
 
-  def prunedCandidates2 = candidates.filter(x => {
+  def prunedCandidates2 = candidates.zipWithIndex.filter({case (x,i) => {
     val otherCount = theOtherSide.count(x._1)
+    val c = x._2
     val backwards = theOtherSide.backwards(x._1)
-    backwards.indices.find(i => backwards(i)._1 == this.word).map(i => i < 5).getOrElse(false)
-  })
+    10 * c > otherCount &&
+      i < 5 &&
+      backwards.indices.find(i => backwards(i)._1 == this.word)
+        .map(i => i < 5).getOrElse(false)
+  }}).map(_._1)
 }
 
 case class Mapped(words: Seq[Word]) {
@@ -38,12 +54,24 @@ case class Mapped(words: Seq[Word]) {
   def backwards(s: String): Seq[(String, Int)] = map.get(s).map(_.candidates).getOrElse(List())
 }
 case class pruneCandidates(fileName: String)  {
-  val candidates  = io.Source.fromFile(fileName).getLines.map(l => l.split("\t")).map(a =>
+
+  def getSource(f: String) = if (f.endsWith(".gz")) io.Source.fromInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(f)))) else io.Source.fromFile(f)
+
+  def filter(t: (String,String,Int))  = t._1.toLowerCase == t._1 && t._2.toLowerCase == t._2 &&
+    t._1.replaceAll("\\p{P}","") == t._1 && t._2.replaceAll("\\p{P}","") == t._2 &&
+    t._1.replaceAll("[0-9]","") == t._1 && t._2.replaceAll("[0-9]","") == t._2
+
+  lazy val candidates: Seq[(String, String, Int)] = getSource(fileName).getLines.map(l => l.split("\t")).map(a =>
     (a(0), a(1), a(2).toInt)
-  ).toList
+  ).toList.filter(filter)
+
   val nullMap = Mapped(Seq())
-  lazy val leftWords0: Seq[Word] = { Console.err.println(candidates.size); candidates.groupBy(_._1).map({case (w,l) => Word(nullMap, w, l.map(l => l._2 -> l._3).sortBy(-1 * _._2))}).toList.sortBy(-1 * _.count) }
-  lazy val rightWords0: Seq[Word] = { Console.err.println(candidates.size); candidates.groupBy(_._2).map({case (w,l) => Word(nullMap, w, l.map(l => l._1 -> l._3).sortBy(-1 * _._2))}).toList.sortBy(-1 * _.count) }
+
+  lazy val leftWords0: Seq[Word] =
+    candidates.groupBy(_._1)
+      .map({case (w,l) => Word(nullMap, w, l.map(x => x._2 -> x._3).sortBy(-1 * _._2).toList)})
+      .toList.sortBy(-1 * _.count)
+  lazy val rightWords0: Seq[Word] =  candidates.groupBy(_._2).map({case (w,l) => Word(nullMap, w, l.map(l => l._1 -> l._3).sortBy(-1 * _._2).toList)}).toList.sortBy(-1 * _.count)
 
   val leftWordMap = Mapped(leftWords0)
   val rightWordMap = Mapped(rightWords0)
--- src/main/scala/corpusprocessing/papiaments/nllb/tmxWordAlign.scala
+++ src/main/scala/corpusprocessing/papiaments/nllb/tmxWordAlign.scala
@@ -5,67 +5,38 @@ import java.util.zip.GZIPInputStream
 import scala.xml._
 import corpusprocessing.clariah_training_corpora.patch_scripts.fixTokenization.getId
 
-import scala.collection.immutable
+import scala.collection.{Seq, immutable}
 import scala.sys.process._
 import scala.xml._
 import java.io._
-case class tmxWordAlign(tmxDoc: Iterator[Elem]) {
-  val genesis = "/mnt/Projecten/Corpora/Historische_Corpora/EDGeS_historical_bible_corpus/XMLConversie/alignment/en_1890_Darby.Gen--nl_1637_Staten.Gen.aligments.xml"
 
+// https://github.com/sillsdev/giza-py
+case class Line(w1: Seq[String], w2:Seq[String], id1:Seq[String] = Seq(), id2: Seq[String] = Seq())
+object wordAlign {
 
+  def reassembleLines(lines: Stream[String], pharaohLines: Stream[String], separator: String="\\|\\|\\|"): Unit = {
+   val tokenizedSegments = lines.map(line => {
+     val (l, r) = line.split(separator) match {
+       case Array(a: String,b: String) =>
 
-  def getLang(w: Node) = w.attributes.filter(_.key == "lang").value.text
+         (a.trim.split("\\s+"),b.split("\\s+"))
+     }
+     val x = Line(l.filter(_.nonEmpty),r.filter(_.nonEmpty))
+     // println(x)
+     x
+    })
+    reassemble(tokenizedSegments, pharaohLines)
+  }
 
+  def reassembleText(textFile: String, pharaohFile: String, separator: String="\\|\\|\\|"): Unit  = {
+    reassembleLines(io.Source.fromFile(textFile).getLines().toStream, io.Source.fromFile(pharaohFile).getLines().toStream, separator)
+  }
 
-  def tokenizedText(v: Node) = v.descendant.filter(x => Set("w", "pc").contains(x.label)).map(_.text.replaceAll("\\s+", "")).toSeq // .mkString(" ")
-
-  def addWordAlignment() = {
-
-    println("Youp!")
-    // deze twee hoeven niet iedere keer
-
-    val linkedVerses: Iterator[(Node, Node)] = (tmxDoc.flatMap(x => x \\ "tuv")).map(tu => {
-      //println(tu)
-      val tuvs = (tu \ "tu")
-      if (tuvs.size >= 1)
-        tuvs(0) -> tuvs(1)
-      else
-          <nope/> -> <nope/>
-    }).filter({ case (x, y) => x.label != "nope" })
-
-
-    // write input file for fastAlign
-    val alignmentFile = new PrintWriter("/tmp/bible.alignMe.txt")
-
-    val tokenizedSegments: Stream[(Seq[String], Seq[String], Seq[String], Seq[String])] =
-      linkedVerses.map({ case (v1, v2) => {
-        val t1: Seq[String] = tokenizedText(v1)
-        val t2 = tokenizedText(v2)
-        val ids1 = (v1 \\ "w").map(getId)
-        val ids2 = (v2 \\ "w").map(getId)
-        alignmentFile.println(s"${t1.mkString(" ")} ||| ${t2.mkString(" ")}")
-        (t1, t2, ids1, ids2)
-      }
-      }).toStream
-
-    println(s"${tokenizedSegments.size}")
-
-    val t1 = tokenizedSegments.map(_._1).toStream
-    val t2 = tokenizedSegments.map(_._2).toStream
-    val ids1 = tokenizedSegments.map(_._3).toStream
-    val ids2 = tokenizedSegments.map(_._4).toStream
-
-    alignmentFile.close()
-
-    // run fastAlign
-
-    val command = "fast_align -i /tmp/bible.alignMe.txt -N -d -o -v -I 10".split("\\s+").toSeq
-    val lines: Stream[String] = command lineStream;
-
-    val allPairs = lines.zip(tokenizedSegments).flatMap({
-      case (line, (w1, w2, id1, id2)) => {
+  def reassemble(tokenizedSegments: Seq[Line], pharaohLines: Stream[String]): Unit = {
+    val allPairs = pharaohLines.zip(tokenizedSegments).flatMap({
+      case (line, Line(w1, w2, id1, id2)) => {
         // println(s"$line $w1 $w2")
-        val pairings = line.trim.split("\\s+").filter(_.contains("-")).map(p => {
+        val pairings: Array[(Int, Int)] = line.trim.split("\\s+").filter(_.contains("-")).map(p => {
           //println(p)
           val lr = p.split("-")
           lr(0).toInt -> lr(1).toInt
@@ -88,30 +59,72 @@ case class tmxWordAlign(tmxDoc: Iterator[Elem]) {
     val pw = new PrintWriter("/tmp/pairs.out")
     lexicon.toList.sortBy(-1 * _._2).foreach({ case ((a, b), c) => pw.println(s"$a\t$b\t$c") })
     pw.close()
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    //lines.foreach(println)
-    val alignedWordIds: Seq[(String, Seq[String])] = decodeAlignment.decodeAlignment(lines, ids1, ids2)
-
-    //alignedWordIds.foreach(println)
-
-    println(s"Found ${alignedWordIds.size} word alignments in ${(tmxDoc \\ "w").size} tokens")
-
-    val wordLinks = alignedWordIds.flatMap({case (l,r) => r.map(r1 => <link type="word-alignment" target={s"#$l #$r1"}/>)})
-   */
-
-
-    // fast_align -i /tmp/bible.alignMe -N -d -o -v -I 10 > forward.align
   }
 
+  val defaultArgs=Array("/mnt/Scratch/jesse/bible.alignMe.500000.txt", "/mnt/Scratch/jesse/alignment")
+  def main(args: Array[String])  = {
+    val a = if (args.size >= 2) args else defaultArgs
+    reassembleText(a(0), a(1))
+  }
+}
+
+import wordAlign._
+case class tmxWordAlign(tmxDoc: Iterator[Elem], parallelTextFile: String="/tmp/bible.alignMe.txt") {
+
+  type line = (Seq[String], Seq[String], Seq[String], Seq[String])
+
+  val genesis = "/mnt/Projecten/Corpora/Historische_Corpora/EDGeS_historical_bible_corpus/XMLConversie/alignment/en_1890_Darby.Gen--nl_1637_Staten.Gen.aligments.xml"
+
+  def getLang(w: Node) = w.attributes.filter(_.key == "lang").value.text
+
+
+  def tokenizedText(v: Node) = v.descendant.filter(x => Set("w", "pc").contains(x.label)).map(_.text.replaceAll("\\s+", "")).toSeq // .mkString(" ")
+
+
+  def fastAlign(): Stream[String] = {
+    val command = s"fast_align -i $parallelTextFile -N -d -o -v -I 10".split("\\s+").toSeq
+    command lineStream
+  }
+
+  def addWordAlignment() = {
+
+    println("Youp!")
+    // deze twee hoeven niet iedere keer
+
+    val linkedVerses: Iterator[(Node, Node)] = (tmxDoc.flatMap(x => x \\ "tuv")).map(tu => {
+      //println(tu)
+      val tuvs = (tu \ "tu")
+      if (tuvs.size >= 1)
+        tuvs(0) -> tuvs(1)
+      else
+          <nope/> -> <nope/>
+    }).filter({ case (x, y) => x.label != "nope" })
+
+
+    // write input file for fastAlign
+
+    lazy val alignmentFile = new PrintWriter(parallelTextFile)
+    val tokenizedLines: Stream[Line] =
+      linkedVerses.map({ case (v1, v2) => {
+        val t1: Seq[String] = tokenizedText(v1)
+        val t2 = tokenizedText(v2)
+        val ids1 = (v1 \\ "w").map(getId)
+        val ids2 = (v2 \\ "w").map(getId)
+        alignmentFile.println(s"${t1.mkString(" ")} ||| ${t2.mkString(" ")}")
+        Line(t1, t2, ids1, ids2)
+      }
+      }).toStream
+
+    println(s"${tokenizedLines.size}")
+
+
+    alignmentFile.close()
+
+    // run fastAlign
+
+
+
+    val pharaohLines = fastAlign()
+    reassemble(tokenizedLines, pharaohLines)
+  }
 }
