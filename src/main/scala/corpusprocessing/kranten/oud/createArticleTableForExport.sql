5bbb47a64b5a2f815e1eeb97fffab60d7d973651
JessedeDoes <dedoes@xs4all.nl>
2024-02-16 09:48:16 +0100

    minor

---
+++ src/main/scala/corpusprocessing/kranten/oud/createArticleTableForExport.sql
@@ -0,0 +1,59 @@
+start transaction;
+drop table if exists articles_int_extra;
+
+create table articles_int_extra (
+        record_id text primary key,
+        kb_article_id text,
+        kb_issue text,
+        subissue text,
+        kb_page text,
+        colophon text,
+        issue_date date,
+        paper_title text,
+        land text,
+        plaats text,
+        tekstsoort text,
+        header text,
+        subheader text,
+        article_text text
+);
+
+insert into articles_int_extra
+select
+  a.record_id,
+  a."article_scannr_KB",
+  a.kb_issue,
+  p.subissue,
+  a.kb_page,
+  trim(i.colophon_int),
+  i.datum_issue,
+  i.paper_title_int,
+  a.land_nw,
+  a.plaats_nw,
+  case
+     when a.tekstsoort_int is not null then lower(a.tekstsoort_int)
+     else lower(a.tekstsoort_nw)
+  end,
+  trim(a.header_int),
+  trim(a.subheader_int),
+  trim(a.article_text_int)
+from
+   "Krantenmetadata17eeeuwdefintieveversie1-22021nw" a,
+   pages_kb p,
+   issues_kb_fixed i
+where
+  not kan_weg and
+  a.article_text_int is not null and
+  (not trim(a.article_text_int) ~ '^\s*$') and
+  a.kb_page=p.kb_page and
+  p.kb_issue=i.kb_issue and
+  p.subissue=i.subissue and
+  i.issue_handled and (not i.wegermee) and i.datum_issue is not null
+  and not (record_id in (select record_id from commentaar_bewerker where kan_weg or issue_mismatch))
+  ;
+
+delete from articles_int_extra where record_id in (select record_id from articles_int);
+
+select count(*) from articles_int_extra;
+rollback;
+-- select count(*) from  "Krantenmetadata17eeeuwdefintieveversie1-22021nw";
--- src/main/scala/corpusprocessing/papiaments/woordenboeken/WoordenboekBankoDiPalabra.scala
+++ src/main/scala/corpusprocessing/papiaments/woordenboeken/WoordenboekBankoDiPalabra.scala
@@ -12,9 +12,38 @@ object WoordenboekBankoDiPalabra {
   val p = new PrettyPrinter(200,4)
   val papStyle = "font-size:10.0pt;color:#231f20;white-space:pre-wrap;"
   val betNrStyle = "font-family:'Times New Roman';font-size:10.0pt;font-weight:bold;color:#231f20;white-space:pre-wrap;"
-  def isBetNr(n: Node) = rend(n) == betNrStyle && n.text.trim.matches("[0-9]+")
+  val posStyle = "font-size:10.0pt;font-style:italic;color:#231f20;white-space:pre-wrap;"
+  val collocStyle = "font-size:10.0pt;font-style:italic;color:#231f20;white-space:pre-wrap;"
 
+  val pos = "(bw|bnw|vnw|voegw|lidw|tw|ww|hulpww|koppelww|vz)"
+  def isBetNr(n: Node) = rend(n) == betNrStyle && n.text.trim.matches("[0-9]+")
+  def isPos(n: Node) = rend(n) == posStyle && (n.text.trim.matches("\\(([a-z]+)\\)") || n.text.trim.matches(s"(.* |^\\()($pos)\\..*"))
+  def isGender(n: Node) = rend(n) == posStyle && (n.text.trim.matches("\\((de|het)\\)"))
+
+  def isColloc(n: Node) = rend(n) == collocStyle && n.text.contains("~")
+  def isLabel(n: Node)  = rend(n) == posStyle && (n.text.trim.matches("\\((.*)\\)"))
+
+
+  def splitEntry(n: Node)  = {
+
+  }
+  def splitRoman(n: Node) = {
+    val t = n.text
+    val regex = "(^|.* )(I|II|III|IV|V|VI|VII|VIII|IX)( |$)"
+    if (t.matches(regex)) {
+
+      val t1 = t.replaceAll(regex,"$1")
+      val t2 = t.replaceAll(regex,"$2")
+      // println(s"Mooi $t ==> <$t1> <$t2>")
+      Seq(n.asInstanceOf[Elem].copy(child = Text(t1.trim)), <blockMarker>{t2.trim}</blockMarker>)
+    } else  {
+      if (t.contains("II")) println(t)
+      n
+    }
+  }
   def rend(n: Node) = (n \ "@rend").text
+
+  def hasLemma(n: Node): Boolean =  n.descendant.exists(x => x.label=="form" && (x \ "@type").text == "lemma")
 
   def groupRenditions(nodes: Seq[Node])  = {
 
@@ -25,15 +54,45 @@ object WoordenboekBankoDiPalabra {
     groups.map(g => <hi rend={rend(g.head)}>{g.text}</hi>)
   }
 
+  def groupSenses(entry: Elem)  = {
+    val groups = grouping.groupWithFirst[Node](entry.child.filter(_.isInstanceOf[Elem]), x => x.label=="senseMarker")
+    val newChildren = groups.flatMap(g => if (g.exists(_.label=="senseMarker")) Seq(<sense>{g}</sense>) else g)
+    entry.copy(child = newChildren)
+  }
+
+  def groupBlocks(entry: Elem)  = {
+    val groups = grouping.groupWithFirst[Node](entry.child.filter(_.isInstanceOf[Elem]), x => x.label == "blockMarker")
+    val newChildren = groups.flatMap(g => if (g.exists(_.label == "blockMarker")) Seq(<sense type="roman">
+      {g}
+    </sense>) else g)
+    entry.copy(child = newChildren)
+  }
+
+  def groupParagraphsintoEntries(div: Elem)  = {
+    val groups = grouping.groupWithFirst(div.child.filter(_.isInstanceOf[Elem]), hasLemma)
+    div.copy(child = groups.map(g => <entry>{g.flatMap(x => x.child)}</entry>))
+  }
+
   def postProcessP(p: Elem)  = {
-    val groupedChildren = groupRenditions(p \ "hi").map(x =>
-      if (rend(x) == papStyle) x.copy(label="papiamentu")
-      else if (isBetNr(x)) x.copy(label="senseMarker") else x)
+    val groupedChildren = groupRenditions(p \ "hi").flatMap(x =>
+      if (rend(x) == papStyle)
+        splitRoman(<form type="papiamentu">{x.child}</form>)
+      else if (isBetNr(x))
+        <senseMarker>{x.text.trim}</senseMarker>
+      else if (isPos(x))
+        <pos>{x.child}</pos>
+      else if (isGender(x))
+        <gram type="gender">{x.child}</gram>
+      else if (isLabel(x))
+        <label>{x.child}</label>
+      else if (isColloc(x))
+        <colloc>{x.child}</colloc>
+      else x)
 
     val g1 = if (groupedChildren.size > 0) {
       val head = groupedChildren(0)
       if (rend(head).contains("bold")) {
-        <lemma>{head.child}</lemma> +: groupedChildren.tail
+        <form type="lemma">{head.child}</form> +: groupedChildren.tail
       } else groupedChildren
 
     }  else groupedChildren
@@ -45,7 +104,12 @@ object WoordenboekBankoDiPalabra {
     val grouped = postProcessP(paragraphs(200).asInstanceOf[Elem])
     // println(p.format(grouped))
 
-    val postProcessed = updateElement(doc, _.label=="p", postProcessP)
-    println(p.format(postProcessed))
+    val post0 = updateElement(doc, _.label=="p", postProcessP)
+    val post1 = updateElement(post0, _.label=="div", groupParagraphsintoEntries)
+    val post2 = updateElement(post1, _.label=="entry", groupBlocks)
+    val post3 = updateElement(post2, _.label=="entry", groupSenses)
+    val post4 = updateElement(post3, _.label=="sense", groupSenses)
+    XML.save("/tmp/dict.xml", post4)
+    // println(p.format(post1))
   }
 }
