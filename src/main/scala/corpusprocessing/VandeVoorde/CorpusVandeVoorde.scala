43b3aa03ed6758e8b841a61d8ce64115ecbbc15f
JessedeDoes <dedoes@xs4all.nl>
2024-05-03 17:43:27 +0200

    minor

--- src/main/scala/corpusprocessing/VandeVoorde/CorpusVandeVoorde.scala
+++ src/main/scala/corpusprocessing/VandeVoorde/CorpusVandeVoorde.scala
@@ -1,21 +1,24 @@
 package corpusprocessing.VandeVoorde
 import scala.xml._
-import java.io.{File,PrintWriter}
+import java.io.{File, PrintWriter}
 import zeeuws.HTML
 import utils.PostProcessXML
+import corpusprocessing.clariah_training_corpora.moderne_tagging.lassy.grouping.groupWithFirst
+import corpusprocessing.edges.openEdges.prettyPrinting
+import corpusprocessing.kranten.oud.TEIScope._
+
 object CorpusVandeVoorde {
    val location = "/mnt/Projecten/Corpora/Historische_Corpora/VandeVoorde_HistoricalCorpusDutch/Corpus - huidige versie/"
    val xmlLoc = "/mnt/Projecten/Corpora/Historische_Corpora/VandeVoorde_HistoricalCorpusDutch/XML/"
-   lazy val files = new File(location).listFiles().iterator
-   lazy val docs: Iterator[(String, Elem)] = files.map(f => {
+
+  lazy val files = new File(location).listFiles().filter(_.getName.endsWith(".txt")).iterator
+
+  lazy val docs: Iterator[(String, Elem)] = files.map(f => {
      val content = io.Source.fromFile(f).getLines().mkString("\n")
-     val content1 = content.replaceAll("</header>","</header>\n<text>\n<body>\n<div>")
+     val content1 = content.replaceAll("</header>","</header>\n<text>\n<body>\n<div>").replaceAll("\\s*\n\\s*\n\\s*", "\n<p/>")
      val pseudoc = s"<TEI>${content1}</div></body></text></TEI>"
      val soupdoc = HTML.parse(pseudoc)
-
-
      f.getName -> soupdoc.asInstanceOf[Elem]
-
    })
 
   /*
@@ -38,23 +41,58 @@ object CorpusVandeVoorde {
       name -> value
     }).toMap
     def m(f: String)  = fields.getOrElse(f, "undefined")
-    def interp(n: String, v: String)  =  { <interpGrp type={n}><interp>{v}</interp></interpGrp> }
+    def interp(n: String, v: String)  =  { <interpGrp type={n.replaceAll("\\s+","_")}><interp>{v}</interp></interpGrp> }
+
     <teiHeader>
-      <title>{m("document")}</title>
-      <sourceDesc>
-        <listBibl>
+      <fileDesc>
+        <titleStmt>
+          <title>{m("document").replaceAll("_", " ")}  ({m("genre")}), {m("place")}, {m("date")}</title>
+          <respStmt>
+            <resp>Transcription, encoding</resp>
+            <name>Iris Van de Voorde</name>
+          </respStmt>
+        <respStmt>
+          <resp>Conversion, linguistic annotation</resp>
+          <name>Instituut voor de Nederlandse Taal</name>
+        </respStmt>
+      </titleStmt>
+        <publicationStmt>
+          <publisher>Universiteit Leiden</publisher>
+          <pubPlace>Leiden</pubPlace>
+          <date>2024</date>
+          <availability>
+            <p>Data at taalmaterialen.ivdnt.org</p>
+          </availability>
+          <availability>
+            <p>In online corpus retrieval application: https://corpusvandevoorde.ivdnt.org</p>
+          </availability>
+        </publicationStmt>
+        <sourceDesc>
+        <listBibl type="originalMetadata">
           <bibl>
             {fields.map({case (n,v) => interp(n,v)})}
           </bibl>
         </listBibl>
       </sourceDesc>
+      </fileDesc>
     </teiHeader>
+  }
+
+  def markParagraphs(div: Elem) = {
+    val groupedChildren = groupWithFirst[Node](div.child, _.label=="p")
+    val paragraphs = groupedChildren.flatMap(g => {
+      if (g.flatMap(_.text).mkString("").trim.nonEmpty) {
+        Seq(<p>{g.filter(_.label != "p")}</p>)
+      } else g.filter(_.label != "p")
+    })
+    div.copy(child = paragraphs)
   }
   implicit def t(x: String): Node => Boolean = _.label == x
   implicit def f(x: String): Elem => Elem = _.copy(label=x)
   implicit def c(x: Elem): Elem => Elem = e => x.copy(child=e.child)
 
   val transformations: List[(Node => Boolean,  Elem => Elem)] = List(
+    t("div") -> markParagraphs,
     t("header") -> parseHeader,
     t("ins") -> <add/>,
     t("cancel") -> <gap reason="cancellation"/>,
@@ -66,10 +104,7 @@ object CorpusVandeVoorde {
     }),
     t("reg") -> (i => {
       val orig = (i \ "@orig").text
-        <choice>
-          <orig>{orig}</orig>
-          <reg>{i.child}</reg>
-        </choice>
+        <choice><orig>{orig}</orig> <reg>{i.child}</reg></choice>
     }),
     t("ed") -> (i => {
       val com = (i \ "@com").text
@@ -84,6 +119,7 @@ object CorpusVandeVoorde {
     t("rightmargin") -> <note place="margin"/>,
     t("name") -> <name type="person"/>,
     t("pl") -> <name type="place"/>,
+    t("ambig") -> <unclear/>
   )
 
   def processTextNode(t: Text): NodeSeq = {
@@ -114,12 +150,18 @@ object CorpusVandeVoorde {
 
   def transForm(d: Elem)  = {
     val z1 = transformations.foldLeft(d)({case (b, (t1,t2)) => PostProcessXML.updateElement(b,t1, t2)})
-    processTextIn(z1)
+    setTeiScope(processTextIn(z1).asInstanceOf[Elem])
   }
   def main(args: Array[String])  = {
     val d = new File(xmlLoc)
     d.mkdir()
     d.listFiles().foreach(f => f.delete)
-    docs.foreach({case (f,d) => XML.save(xmlLoc + f.replaceAll("txt$", "xml"), transForm(d))})
+
+    docs.foreach({ case (f, d) =>
+      val t: Elem = transForm(d)
+      val pw = new PrintWriter(xmlLoc + f.replaceAll("txt$", "xml"))
+      prettyPrinting.prettyScala(pw, t)
+      pw.close()
+    })
   }
 }
