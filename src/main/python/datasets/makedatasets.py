675f0acd715c78115d5fd9850581b95d7f025165
JessedeDoes <dedoes@xs4all.nl>
2024-05-31 09:36:34 +0200

    minor

---
+++ src/main/python/datasets/makedatasets.py
@@ -0,0 +1,39 @@
+import json
+import os
+import sys
+import re
+
+t = open("template.json", "r")
+template=json.load(t)
+template['tagset'] = 'TDN-Core'
+template["version"] = '0.9'
+template["columns"] = ["token", "pos", "lemma", "group_id"]
+
+f = open('lijstje.tsv','r')
+
+lines = f.readlines()
+datasets = []
+
+def clone(x):
+  return json.loads(json.dumps(x))
+
+for l in lines:
+  cols = re.split('\t', l.strip())
+  #print(cols)
+  [name, desc, code, f, t, url] = cols
+  instance = clone(template)
+  instance['trainingPath'] = f"training-data/{name}"
+  instance['eraFrom'] = f
+  instance['eraTo'] = t
+  instance['name'] = name
+  instance['sourceName'] = name
+  instance['sourceURL'] = url # "http://github.com/INL/galahad-corpus-data"
+  instance['sourcePath'] = f'source-data/{name}'
+  instance['description'] = desc
+  datasets.append(instance)
+
+  fout=open(f"sets/{name}.json", "w")
+  json.dump(instance,fout,indent=4)
+  fout.close()
+
+print(json.dumps(datasets,indent=4))
