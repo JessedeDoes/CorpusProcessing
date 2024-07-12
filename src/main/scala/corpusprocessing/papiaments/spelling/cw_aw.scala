19436ad9cfbb7daeafce1c11da0f0b3fc06e5744
JessedeDoes <dedoes@xs4all.nl>
2024-07-12 19:41:33 +0200

    minor

--- src/main/scala/corpusprocessing/papiaments/spelling/cw_aw.scala
+++ src/main/scala/corpusprocessing/papiaments/spelling/cw_aw.scala
@@ -1,7 +1,7 @@
 package corpusprocessing.papiaments.spelling
 
+import java.io.PrintWriter
 import scala.util.matching.Regex.Match
-
 import java.text.Normalizer
 import java.text.Normalizer.Form
 
@@ -16,11 +16,17 @@ import DiacriticRemover._
 object cw_aw {
 
   val baseDir = "/mnt/Projecten/Papiaments/Woordenboeken/ArubaLijst/"
+
+  val gerundio_list = baseDir + "gerundio_cw.txt" // "copy (select distinct wordform from paradigm where part_of_speech ~ '=ger'  and lemma_id in (select lemma_id from lemmata where language_id='pap_cw')) to '/tmp/gerundio.txt';"
   lazy val arubalijst = io.Source.fromFile(baseDir + "unieke_lijst.txt").getLines.map(x => x.replaceAll(" .*", "")).toSet.filter(!_.contains("["))
   lazy val alignments = io.Source.fromFile(baseDir + "../../Alignments/pairs.pruned.e2p.out").getLines
   val buki = baseDir + "../GoudenBoekje/buki_plus_retro.tab"
-  lazy val bukiwords = io.Source.fromFile(buki).getLines.map(_.replaceAll("\t.*","")).filter(_.nonEmpty).toSet
+
+  lazy val bukiwords = io.Source.fromFile(buki).getLines.map(_.replaceAll("\t.*","")).filter(_.nonEmpty).toSet ++ io.Source.fromFile(gerundio_list).getLines.toSet
+
+
   lazy val approvedBukiWords =  io.Source.fromFile(buki).getLines.filter(_.contains("buki")).map(_.replaceAll("\t.*","")).filter(_.nonEmpty).toSet
+
   lazy val bukiwords_noacc = bukiwords.map(removeDiacritics)
 
   lazy val bukiReaccent: Map[String, Set[String]] = bukiwords.map(x => removeDiacritics(x) -> x).groupBy(_._1).mapValues(_.map(_._2))
@@ -50,6 +56,7 @@ object cw_aw {
     "sci" -> "si",
     "xi" -> "si",
     "x" -> "ks",
+    "x" -> "s",
     "th" -> "t",
     "[pbk]s" -> "s",
     // assimilaties
@@ -62,10 +69,12 @@ object cw_aw {
     "^ps" -> "s",
     "ss" -> "s",
     "ie" -> "e",
-
+    "au" -> "ou",
     "d$" -> "t",
     "cu" -> "kw",
-    "b" -> "v"
+    "b" -> "v",
+    "dor$" -> "do",
+    "y" -> "i"
   )
 
   def tryAll(source: String, pat: String, replacement: String) = {
@@ -89,12 +98,13 @@ object cw_aw {
     if (arubalijst.contains(w)) Set() else blaasop(Set(w)).filter(x => arubalijst.contains(x))
   }
 
-  val N = Integer.MAX_VALUE
+  val N = 1000 // Integer.MAX_VALUE
   def pick(s: Set[String], n:Int=N) = util.Random.shuffle(s.toList).take(n).toSet
 
   def reconstruct(x: String): Set[String] = prefer(bukiReaccent(x), y => approvedBukiWords.contains(y)  )
   def fromAruba() = {
 
+    val tsvTje = new PrintWriter("/tmp/match.tsv")
 
     val matches = pick(arubalijst).toList.sorted.map(w0 => {
       val w = removeDiacritics(w0)
@@ -103,9 +113,10 @@ object cw_aw {
       if (found.nonEmpty) {
         val r = prefer(found.flatMap(reconstruct),x => approvedBukiWords.contains(x))
         println(w + " -> " + r.mkString(", "))
+        tsvTje.println(s"$w\t${r.mkString(", ")}")
       } else {
         println(w + " -> " + "______") //   + cands)
-
+        tsvTje.println(s"$w\t")
       }
       w0 -> found
     })
@@ -113,6 +124,7 @@ object cw_aw {
     val trivial = matches.filter({case (p,v) => v.contains(p)}).size
     val percentage = Math.round(10000 * (nMatches / matches.size.toDouble)).toDouble / 100
     println(s"matched $nMatches of ${matches.size} ($percentage), $trivial identical matches")
+    tsvTje.close()
   }
 
   def main(args: Array[String])  = fromAruba()
