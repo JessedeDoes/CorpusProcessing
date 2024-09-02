da9b7d7588853b08195bd370f36f8512172ea409
JessedeDoes <dedoes@xs4all.nl>
2024-09-02 09:28:49 +0200

    minor

--- src/main/scala/corpusprocessing/GCND/checkOpname.scala
+++ src/main/scala/corpusprocessing/GCND/checkOpname.scala
@@ -2,17 +2,21 @@ package corpusprocessing.GCND
 
 import scala.xml._
 object checkOpname {
-  val dir = "/mnt/Projecten/Hercules/Corpus-ZNL-Dialecten/DownloadFinaalAugustus2024/opnames"
-
+  val baseDir = "/mnt/Projecten/Hercules/Corpus-ZNL-Dialecten/DownloadFinaalAugustus2024/"
+  val dir = s"$baseDir//opnames"
+  val oggdir = s"$baseDir/oggfromp3/"
+  val mp4dir = s"$baseDir/opnames_mp4/"
   lazy val allMp3Files = new java.io.File(dir).listFiles().map(_.getName).toSet.filter(_.endsWith(".mp3"))
-
+  lazy val allOggFiles = new java.io.File(oggdir).listFiles().map(_.getName).toSet.filter(_.endsWith(".ogg"))
+  lazy val allMp4Files = new java.io.File(mp4dir).listFiles().map(_.getName).toSet.filter(_.endsWith(".mp4"))
   lazy val allWavjes = io.Source.fromFile("/mnt/Projecten/Hercules/Corpus-ZNL-Dialecten/DownloadFinaalAugustus2024/wav.lst").getLines().map(w => w.replaceAll(".*/", "").toLowerCase -> w).toMap
 
   lazy val foliaDir = "/home/jesse/workspace/XmlToRdf/data/GCND/Folia"
+
   lazy val foliaFiles = new java.io.File(foliaDir).listFiles().iterator.map(XML.loadFile)
   lazy val codes  = foliaFiles.map(f => (f \\ "opname_code").text).toSet
 
-  lazy val missing = codes.filter(c => !allMp3Files.contains(c + ".mp3"))
+  lazy val missing = codes.filter(c => !allMp4Files.contains(c + ".mp4"))
 
   def main(args: Array[String]) = {
     println(allWavjes)
