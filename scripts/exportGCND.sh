a83e76110e32baaa51ad630a6321cc29bbbe7627
Jesse <dedoes@xs4all.nl>
2024-10-14 17:01:03 +0200

    minor

---
+++ scripts/exportGCND.sh
@@ -0,0 +1,2 @@
+export CLASSPATH=./target/scala-2.12/CorpusProcessing-assembly-0.1.jar 
+java -Xmx12g corpusprocessing.GCND.GCNDDatabase
--- src/main/scala/corpusprocessing/GCND/Preparation.scala
+++ src/main/scala/corpusprocessing/GCND/Preparation.scala
@@ -1,10 +1,41 @@
 package corpusprocessing.GCND
 
 object Preparation {
-  val queries =
-    """start transaction;
+
+
+  val alpino_elan_stricter =
+    """
       |drop view if exists elan_2_alpino cascade;
       |drop view if exists elan_2_alpino_unique;
+      |
+      |create index if not exists et_index on elan_annotatie(transcriptie_id);
+      |create index if not exists at_index on alpino_annotatie(transcriptie_id);
+      |
+      |create temporary table elan_2_alpino_stricter as
+      |        select e.transcriptie_id, elan_annotatie_id, alpino_annotatie_id, e.opname__persoon_id as pe, a.opname__persoon_id as pa,
+      |        case
+      |          when a.starttijd >= e.starttijd and a.starttijd < e.eindtijd then least(e.eindtijd,a.eindtijd) - a.starttijd
+      |          when a.eindtijd > e.starttijd and a.eindtijd <= e.eindtijd then a.eindtijd - greatest(a.starttijd, e.starttijd)
+      |          else 0
+      |        end as overlap
+      |        from elan_annotatie e, alpino_annotatie a
+      |        where e.transcriptie_id = a.transcriptie_id and
+      |        ((a.starttijd >= e.starttijd and a.starttijd < e.eindtijd) or (a.eindtijd > e.starttijd and a.eindtijd <= e.eindtijd));
+      |
+      |create view alpino_2_elan_unique as select
+      |        transcriptie_id,
+      |        alpino_annotatie_id,
+      |        (array_agg(elan_annotatie_id order by pe=pa desc, overlap desc))[1] as elan_annotatie_id,
+      |        array_agg(elan_annotatie_id order by pe=pa desc, overlap desc) as options,
+      |        array_agg(overlap order by overlap desc) as overlaps from elan_2_alpino_stricter group by alpino_annotatie_id, transcriptie_id;
+      |
+      |""".stripMargin
+
+  val link_elan_and_alpino_annotations=
+    """
+      |drop view if exists elan_2_alpino cascade;
+      |drop view if exists elan_2_alpino_unique;
+      |
       |create view elan_2_alpino as
       |
       |	select e.transcriptie_id, elan_annotatie_id, alpino_annotatie_id,
@@ -23,6 +54,12 @@ object Preparation {
       |		(array_agg(elan_annotatie_id order by overlap desc))[1] as elan_annotatie_id,
       |		array_agg(elan_annotatie_id order by overlap desc) as options,
       |		array_agg(overlap order by overlap desc) as overlaps from elan_2_alpino group by alpino_annotatie_id, transcriptie_id;
+      |
+      |""".stripMargin
+
+  val queries =
+    s"""start transaction;
+      |$alpino_elan_stricter
       |
       | create table if not exists tagged_tokens (elan_annotatie_id integer, tokens text);
       | drop view if exists elan_annotatie_plus;
