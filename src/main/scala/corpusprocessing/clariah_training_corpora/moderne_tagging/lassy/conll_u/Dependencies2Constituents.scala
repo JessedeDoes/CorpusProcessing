2624e675b12cf9c2e2e8b8b96f530daa0d2fcd4a
JessedeDoes <dedoes@xs4all.nl>
2024-10-30 19:09:53 +0100

    minor

---
+++ src/main/scala/corpusprocessing/clariah_training_corpora/moderne_tagging/lassy/conll_u/Dependencies2Constituents.scala
@@ -0,0 +1,14 @@
+package corpusprocessing.clariah_training_corpora.moderne_tagging.lassy.conll_u
+
+
+/*
+Mogelijkheden
+- Verrijkte informatie tijdens het parsen, daarna reconstructieprocedure (type Hall en Nivre)
+- Reconstructieregels
+  - mogelijk met deterministische CFG parsing
+- Train een probabilistische CFG op het reconstructieprobleem
+ */
+
+object Dependencies2Constituents {
+
+}
