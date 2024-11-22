19fc57b24f776b449339bc270b6cf7af05c47a72
JessedeDoes <dedoes@xs4all.nl>
2024-11-22 14:44:58 +0100

    minor

---
+++ data/TDN/topdf_met_pandoc.sh
@@ -0,0 +1,2 @@
+pandoc TDN_tagset.latest.html -o TDN_tagset.latest.test.pdf -V pagestyle=plain -V geometry:margin=1in -V header-includes="\\setcounter{page}{82}"
+
---
+++ data/TDN/wk.sh
@@ -0,0 +1,2 @@
+wkhtmltopdf --footer-right "Page [page] of [topage]" --footer-font-size 10 --page-offset 81 --margin-bottom 20mm TDN_tagset.latest.html TDN_tagset.latest.test.pdf
+
